51fa3731444b237bdad7e4675e78eff7c07dee90
William Towle <william.towle@codethink.co.uk>
2018-11-12 14:25:40 +0000

    Zaphod Skeleton build for v0.12.5
    Rudimentary Z80 system - build system modifications/skeleton code
    
    Includes final modifications specific to QEmu >= 0.10.x

---
+++ hw/zaphod.h
@@ -0,0 +1,14 @@
+/* Skeleton Z80 machine for QEmu */
+/* Wm. Towle c. 2013-2018 */
+
+#ifndef _ZAPHOD_H_
+#define _ZAPHOD_H_
+
+/* "Zaphod" Z80 machine family */
+
+#define ZAPHOD_DEBUG	1	/* WARNING: uses fprintf() */
+
+/* ZAPHOD_RAM_SIZE: maximum for a Z80 is 64K */
+#define	ZAPHOD_RAM_SIZE	(64 * 1024)
+
+#endif	/*  _ZAPHOD_H_  */
