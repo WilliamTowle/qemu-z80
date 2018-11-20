3f56663ba71d8edb82791c01a331e9509ec39204
William Towle <william_towle@yahoo.co.uk>
2018-11-20 11:47:35 +0000

    Zaphod: configure/build for separate zaphod_sercon

--- hw/zaphod.h
+++ hw/zaphod.h
@@ -14,18 +14,40 @@
 
 #include "exec-memory.h"
 
+/* Feature configuration - see also z80-softmmu.mak */
+
+#define ZAPHOD_HAS_SERCON
+
 /* Z80_MAX_RAM_SIZE:
  * Address space for a Z80 ends at 64K (some emulations might use less)
  */
 #define	Z80_MAX_RAM_SIZE	(64 * 1024) /* 64KiB */
 
+typedef struct ZaphodState ZaphodState;
+
+#ifdef ZAPHOD_HAS_SERCON
 typedef struct {
+    ZaphodState     *super;
+
+    PortioList      *ports;
+    CharDriverState *sercon;            /* QEmu serial0 console */
+} ZaphodSerConState;
+#endif
+
+struct ZaphodState {
     CPUState        *cpu;
     MemoryRegion    *ram;
-    PortioList      *ports;
-
-    CharDriverState *sercon;            /* QEmu serial0 console */
     uint8_t         inkey;
-} ZaphodState;
+
+#ifdef ZAPHOD_HAS_SERCON
+    ZaphodSerConState   *sercon;
+#endif
+};
+
+#ifdef ZAPHOD_HAS_SERCON
+void zaphod_sercon_putchar(ZaphodSerConState *zss, const unsigned char ch);
+
+ZaphodSerConState *zaphod_new_sercon(ZaphodState *zs, CharDriverState* sercon);
+#endif
 
 #endif	/*  _ZAPHOD_H_  */
