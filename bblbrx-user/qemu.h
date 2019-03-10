b892a67e79320efa7d5906935a9ec18b79ab8a48
William Towle <william_towle@yahoo.co.uk>
2019-03-10 11:50:14 +0000

    bblbrx-user headers: abi_{long|ulong} types

--- bblbrx-user/qemu.h
+++ bblbrx-user/qemu.h
@@ -1,7 +1,9 @@
 #ifndef QEMU_H
 #define QEMU_H
 
-#include "config.h"	/* for arch config - normally via cpu.h */
+#include "config.h"    /* for arch config - normally via cpu.h */
+
+#include "qemu-types.h"
 
 int bblbrx_exec(const char *filename);
 
