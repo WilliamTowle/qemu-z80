e824e4b0193742b442064bc0a5402b0bd333b2ec
William Towle <william_towle@yahoo.co.uk>
2019-03-10 12:48:13 +0000

    target-z80: initial cpu.h

--- target-z80/cpu.h
+++ target-z80/cpu.h
@@ -8,8 +8,45 @@
 #ifndef CPU_Z80_H
 #define CPU_Z80_H
 
+//#include "config.h"
+//#include "qemu-common.h"
+
 #define TARGET_LONG_BITS 32
 
+#define CPUArchState struct CPUZ80State
+
 #include "exec/cpu-defs.h"
+
+/* cpu-defs.h needs NB_MMU_MODES */
+#define NB_MMU_MODES 1	/* more in target-x86 */
+
+typedef struct CPUZ80State {
+    /* TODO: Z80 registers */
+
+    CPU_COMMON
+
+    /* TODO: 'int model' CPU identifier */
+} CPUZ80State;
+
+
+
+/* TARGET_PAGE_BITS required by exec-all.h cache */
+#define TARGET_PAGE_BITS 12	/* from target-x86 */
+
+#define TARGET_PHYS_ADDR_SPACE_BITS 32 /* min TCG register size? */
+#define TARGET_VIRT_ADDR_SPACE_BITS 32 /* min TCG register size? */
+
+
+// MMU modes definitions?
+
+static inline int cpu_mmu_index (CPUZ80State *env)
+{
+    return 0;	/* verbatim from target-lm32 */
+		/* maybe MMU_USER_IDX? */
+}
+
+#include "exec/cpu-all.h"
+
+#include "exec/exec-all.h"
 
 #endif /* !defined (CPU_Z80_H) */
