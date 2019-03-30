3bc9b57749864ce899506ea64cb145f99b86fbe7
William Towle <william_towle@yahoo.co.uk>
2019-03-30 19:52:10 +0000

    target-z80: extend cpu_dump_state() implementation

--- target-z80/helper.c
+++ target-z80/helper.c
@@ -14,16 +14,36 @@ void z80_cpu_dump_state(CPUState *cs, FILE *f, fprintf_function cpu_fprintf,
 {
     Z80CPU *cpu= Z80_CPU(cs);
     CPUZ80State *env= &cpu->env;
+    int fl = env->regs[R_F];
 
 #if 1	/* PARTIAL: Full register set not yet implemented */
-    cpu_fprintf(f, "AF =???? BC =???? DE =???? HL =???? IX=????\n"
-                   "AF'=???? BC'=???? DE'=???? HL'=???? IY=????\n"
-                   "PC =%04x SP =???? F=[????????]\n"
+    cpu_fprintf(f, "AF =%04x BC =%04x DE =%04x HL =%04x IX=%04x\n"
+                   "AF'=%04x BC'=%04x DE'=%04x HL'=%04x IY=%04x\n"
+                   "PC =%04x SP =%04x F=[%c%c%c%c%c%c%c%c]\n"
                    "IM=? IFF1=? IFF2=? I=?? R=??\n",
-		    env->pc
+                   (env->regs[R_A] << 8) | env->regs[R_F],
+                   env->regs[R_BC],
+                   env->regs[R_DE],
+                   env->regs[R_HL],
+                   env->regs[R_IX],
+                   (env->regs[R_AX] << 8) | env->regs[R_FX],
+                   env->regs[R_BCX],
+                   env->regs[R_DEX],
+                   env->regs[R_HLX],
+                   env->regs[R_IY],
+		   env->pc,
+                   env->regs[R_SP],
+                   fl & 0x80 ? 'S' : '-',
+                   fl & 0x40 ? 'Z' : '-',
+                   fl & 0x20 ? 'Y' : '-',
+                   fl & 0x10 ? 'H' : '-',
+                   fl & 0x08 ? 'X' : '-',
+                   fl & 0x04 ? 'P' : '-',
+                   fl & 0x02 ? 'N' : '-',
+                   fl & 0x01 ? 'C' : '-'
 		    );
 #else	/* final repo.or.cz implementation */
-    int fl = env->regs[R_F];
+    //int fl = env->regs[R_F];
     cpu_fprintf(f, "AF =%04x BC =%04x DE =%04x HL =%04x IX=%04x\n"
                    "AF'=%04x BC'=%04x DE'=%04x HL'=%04x IY=%04x\n"
                    "PC =%04x SP =%04x F=[%c%c%c%c%c%c%c%c]\n"
@@ -51,3 +71,4 @@ void z80_cpu_dump_state(CPUState *cs, FILE *f, fprintf_function cpu_fprintf,
                    env->imode, env->iff1, env->iff2, env->regs[R_I], env->regs[R_R]);
 #endif
 }
+
