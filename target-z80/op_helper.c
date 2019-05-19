66dbf750f9fb745525b81b738ef90a05d0806054
William Towle <william_towle@yahoo.co.uk>
2019-05-19 14:43:43 +0000

    (z80) exceptions/interrupts: add movl_pc_im()

---
+++ target-z80/op_helper.c
@@ -0,0 +1,36 @@
+/*
+ * Skeleton for minimal QEmu Z80 CPU
+ * vim: ft=c sw=4 ts=4 et :
+ *
+ * WmT, 2018-2022 [...after Stuart Brady, under GPLv2+...]
+ */
+
+
+#include "exec.h"
+#include "helper.h"
+
+//#define A0 (env->a0)
+//
+//#define A   (env->regs[R_A])
+//#define F   (env->regs[R_F])
+//#define BC  (env->regs[R_BC])
+//#define DE  (env->regs[R_DE])
+//#define HL  (env->regs[R_HL])
+//#define IX  (env->regs[R_IX])
+//#define IY  (env->regs[R_IY])
+//#define SP  (env->regs[R_SP])
+//#define I   (env->regs[R_I])
+//#define R   (env->regs[R_R])
+//#define AX  (env->regs[R_AX])
+//#define FX  (env->regs[R_FX])
+//#define BCX (env->regs[R_BCX])
+//#define DEX (env->regs[R_DEX])
+//#define HLX (env->regs[R_HLX])
+
+#define PC  (env->pc)
+
+
+void HELPER(movl_pc_im)(uint32_t new_pc)
+{
+    PC = (uint16_t)new_pc;
+}
