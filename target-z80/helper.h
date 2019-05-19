f45a66afe7480b9b2f9944bbe8ca01907a9f68a6
William Towle <william_towle@yahoo.co.uk>
2019-05-19 14:43:43 +0000

    z80 translate: op_helper.{c|h} jmp_T0

--- target-z80/helper.h
+++ target-z80/helper.h
@@ -8,4 +8,12 @@ DEF_HELPER_0(reset_inhibit_irq, void)
 
 DEF_HELPER_1(movl_pc_im, void, i32)
 
+///* In / Out */
+
+/* Misc */
+
+//DEF_HELPER_1(bit_T0, void, i32)
+DEF_HELPER_0(jmp_T0, void)
+//DEF_HELPER_2(djnz, void, i32, i32)
+
 #include "def-helper.h"
