713986ca11ec132c59c7901f1ebae4a130353d19
William Towle <william_towle@yahoo.co.uk>
2019-07-31 00:32:17 +0000

    z80 translate: initial code gen hooks [v1.7.x]

--- target-z80/translate.c
+++ target-z80/translate.c
@@ -4,3 +4,36 @@
  *
  * WmT, 2018-2022 [...after Stuart Brady, under GPL...]
  */
+
+#include "cpu.h"
+
+
+#if 1	/* debug */
+	/* TODO: version with error_printf() needs CPU headers */
+#define DPRINTF(fmt, ...) \
+	do { fprintf(stderr, fmt, ## __VA_ARGS__); } while (0)
+#else
+#define DPRINTF(fmt, ...) \
+	do { } while (0)
+#endif
+
+///* generate intermediate code in gen_opc_buf and gen_opparam_buf for
+//   basic block 'tb'. If search_pc is TRUE, also generate PC
+//   information for each intermediate instruction. */
+static inline void gen_intermediate_code_internal(Z80CPU *cpu,
+                                                 TranslationBlock *tb,
+                                                 int search_pc)
+{
+;DPRINTF("BAIL %s() - INCOMPLETE\n", __func__);
+;exit(1);
+}
+
+void gen_intermediate_code(CPUZ80State *env, TranslationBlock *tb)
+{
+    gen_intermediate_code_internal(z80_env_get_cpu(env), tb, false);
+}
+
+void gen_intermediate_code_pc(CPUZ80State *env, TranslationBlock *tb)
+{
+    gen_intermediate_code_internal(z80_env_get_cpu(env), tb, true);
+}
