e3b69256ba07ef2afc48d70b95c37c32e3c3cd27
William Towle <william_towle@yahoo.co.uk>
2019-08-02 21:33:59 +0000

    bblbrx-user: skeleton cpu_model handling

--- bblbrx-user/main.c
+++ bblbrx-user/main.c
@@ -57,6 +57,7 @@ static int parse_args(int argc, char **argv)
 
 int main(int argc, char **argv)
 {
+  const char *cpu_model= NULL;
   char *filename;
   void  *target_ram;
   int optind;
@@ -73,13 +74,21 @@ int main(int argc, char **argv)
      * 2. log filename is set to default
      * 3. may need to initialise environment list
      * 4. consider support for cpu model change in arguments
-     * 5. want to manage cpu_model default value if not in args
      */
 
     optind= parse_args(argc, argv);
     if (optind >= argc)
         usage(EXIT_FAILURE);
     filename= argv[optind];
+
+    if (cpu_model == NULL)
+    {
+#if defined(TARGET_Z80)
+        cpu_model = "z80";
+#else
+#error unsupported target CPU
+#endif
+    }
 
 #if 1	/* WmT - TRACE */
 ;fprintf(stderr, "%s(): PARTIAL - missing initialisation 2/3...\n", __func__);
@@ -109,6 +118,7 @@ int main(int argc, char **argv)
      * to ensure any ROM regions can be protected from writes. In
      * the meantime we get to have self-modifying code anywhere.
      */
+
     target_ram= mmap(0, 64*1024,
                      PROT_READ | PROT_WRITE,
                      MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
