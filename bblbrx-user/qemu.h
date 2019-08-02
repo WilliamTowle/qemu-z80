e94d01f10d93caf4319cca9267c18c1b2420fc9a
William Towle <william_towle@yahoo.co.uk>
2019-08-02 22:40:29 +0000

    bblbrx-user: introduce bblbrx_binprm/load_raw_binary()

--- bblbrx-user/qemu.h
+++ bblbrx-user/qemu.h
@@ -12,6 +12,14 @@
 
 #include "qemu-types.h"
 
+struct bblbrx_binprm {
+    const char		*filename;
+    int			fd;
+    long		filesize;
+};
+
 int bblbrx_exec(const char *filename);
+
+int load_raw_binary(struct bblbrx_binprm *bprm);
 
 #endif /* QEMU_H */
