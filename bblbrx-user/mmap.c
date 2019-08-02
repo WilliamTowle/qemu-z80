a5a28f9a6b6ef4242e91cd81ab33fe0aa5fb9eba
William Towle <william_towle@yahoo.co.uk>
2019-08-02 21:33:59 +0000

    bblbrx-user: complete load/execute implementation

---
+++ bblbrx-user/mmap.c
@@ -0,0 +1,11 @@
+#include <sys/mman.h>
+
+#include "qemu.h"
+
+void mmap_lock(void)
+{
+}
+
+void mmap_unlock(void)
+{
+}
--- bblbrx-user/qemu.h
+++ bblbrx-user/qemu.h
@@ -7,14 +7,35 @@
 
 #include "qemu-types.h"
 
+typedef struct TaskState {
+	int used;
+	struct bblbrx_binprm *bprm;
+} TaskState;	/* aligmnent is useful here, for the linux-user case */
+
 struct bblbrx_binprm {
 	const char	*filename;
 	int		fd;
 	long		filesize;
+	target_ulong	magic_ramloc;
 };
 
-int bblbrx_exec(const char *filename);
+#define VERIFY_READ 0
+#define VERIFY_WRITE 1 /* implies read access */
+
+/* XXX: todo protect every memory access */
+#define lock_user(x,a,b,c)	(void*)(x)
+#define unlock_user(x,y,z)
+
+int bblbrx_exec(const char *filename, struct bblbrx_binprm *bprm);
 
 int load_raw_binary(struct bblbrx_binprm *bprm);
+
+void cpu_loop(CPUState *env);
+
+void mmap_lock(void);
+void mmap_unlock(void);
+
+void cpu_list_lock(void);
+void cpu_list_unlock(void);
 
 #endif /* QEMU_H */
