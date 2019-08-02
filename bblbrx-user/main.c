d02bfbd635a6d2e0cc0e0af6912f76e5b605fb37
William Towle <william_towle@yahoo.co.uk>
2019-08-02 21:33:59 +0000

    bblbrx-user: call CPU init, with 'env' addr announce

--- bblbrx-user/main.c
+++ bblbrx-user/main.c
@@ -11,6 +11,7 @@
 #include <sys/mman.h>
 
 #include "qemu.h"
+#include "cpu.h"
 
 
 #if 1	/* debug */
@@ -71,6 +72,7 @@ int main(int argc, char **argv)
 {
   const char *cpu_model= NULL;
     char *filename;
+  CPUState *env;
   void  *target_ram;
     int optind;
   int ret;
@@ -106,16 +108,22 @@ int main(int argc, char **argv)
 ;DPRINTF("%s(): PARTIAL - missing initialisation 2/3...\n", __func__);
 #endif
     /* PARTIAL:
-     * to effect CPU init, we do:
-     *    1. call to cpu_exec_init_all(0);
-     *    2. call env = cpu_init(cpu_model);
-     *    3. maybe a cpu_reset(env) call (TARGET_{I386|SPARC|PPC})
-     *    4. initialise 'thread_env' (externed via qemu.h)
-     *    5. set 'do_strace', if supported
-     *    6. initialise 'target_environ'
-     *    7. handle CONFIG_USE_GUEST_BASE
-     *    8. manage passing arg{c|v} to target, if required
-     *    9. allocate/initialise any TaskState
+     * prior to CPU init, we do:
+     *  1. tcg_exec_init(0);
+     *  2. call to cpu_exec_init_all(0);
+     */
+
+    env = cpu_init(cpu_model);
+    if (!env) {
+        fprintf(stderr, "Unable to find definition for cpu_model '%s'\n", cpu_model);
+        exit(1);
+    }
+
+    /* ...and after cpu_init()
+     *  1. maybe a cpu_reset(env) call (TARGET_{I386|SPARC|PPC})
+     *  2. initialise 'thread_env' (externed via qemu.h)
+     *  3. set 'do_strace', if supported
+     *  4. initialise 'target_environ'
      */
 
 #if !defined(CONFIG_USE_GUEST_BASE)
@@ -143,6 +151,7 @@ int main(int argc, char **argv)
     /* Giving guest_base a value causes ldub_code() to retrieve bytes
      * from addresses that won't segfault
      */
+
     guest_base= (unsigned long)target_ram;
 #if 1	/* WmT - TRACE */
 ;DPRINTF("%s(): INFO - set guest_base=%p\n", __func__, (void *)guest_base);
@@ -153,9 +162,8 @@ int main(int argc, char **argv)
 ;DPRINTF("%s(): PARTIAL - missing initialisation 3/3...\n", __func__);
 #endif
     /* PARTIAL: next...
-     * 1. handle CONFIG_USE_GUEST_BASE
-     * 2. manage passing arg{c|v} to target, if required
-     * 3. initialise any TaskState
+     *  1. manage passing arg{c|v} to target, if required
+     *  2. allocate/initialise any TaskState
      */
 
     ret= bblbrx_exec(filename);
