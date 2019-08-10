c87354a292d36e67f2f68be83ced1505c7590de3
William Towle <william_towle@yahoo.co.uk>
2019-08-10 16:50:13 +0000

    WIP: initial misc_helper.c - migrate debug helper

---
+++ target-z80/misc_helper.c
@@ -0,0 +1,20 @@
+/*
+ * QEmu Z80 CPU - misc helpers
+ * vim: ft=c sw=4 ts=4 et :
+ *
+ * WmT, 2018-2022 [...after Stuart Brady, under GPL...]
+ */
+
+#include "cpu.h"
+#include "helper.h"
+
+
+void helper_debug(CPUZ80State *env)
+{
+    env->exception_index = EXCP_DEBUG;
+#if 0	/* obsolete */
+    cpu_loop_exit();
+#else	/* v0.15.0+ */
+    cpu_loop_exit(env);
+#endif
+}
