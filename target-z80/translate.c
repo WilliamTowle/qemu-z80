6103bbadcecd36bb3db70aeb3268a7fd57c1681c
William Towle <william_towle@yahoo.co.uk>
2019-08-10 16:50:13 +0000

    z80 codegen: gen_eob() and prototype

--- target-z80/translate.c
+++ target-z80/translate.c
@@ -72,9 +72,24 @@ typedef struct DisasContext {
     struct TranslationBlock *tb;
 } DisasContext;
 
+static void gen_eob(DisasContext *s);
+
 static inline void gen_jmp_im(target_ulong pc)
 {
     gen_helper_movl_pc_im(cpu_env, tcg_const_tl(pc));
+}
+
+static void gen_eob(DisasContext *s)
+{
+    if (s->tb->flags & HF_INHIBIT_IRQ_MASK) {
+        gen_helper_reset_inhibit_irq();
+    }
+    if (s->singlestep_enabled) {
+        gen_helper_debug();
+    } else {
+        tcg_gen_exit_tb(0);
+    }
+    s->is_jmp = 3;
 }
 
 
