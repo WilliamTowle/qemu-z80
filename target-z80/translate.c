7a31b6445235ccaa25e1b6edd7e7bda085cf9885
William Towle <william_towle@yahoo.co.uk>
2019-09-22 23:33:27 +0000

    z80 translate: magic_ramtop exits loop, with EXCP_KERNEL_TRAP

--- target-z80/translate.c
+++ target-z80/translate.c
@@ -1179,9 +1179,6 @@ static inline int gen_intermediate_code_internal(CPUState *env,
     gen_icount_start();
     for (;;) {
 #if defined(CONFIG_USER_ONLY) && defined(TARGET_Z80)
-#if 1	/* WmT - TRACE */
-;fprintf(stderr, "[%s:%d] PARTIAL - check magic_ram value (current PC 0x%04x, opaque %p) here?\n", __FILE__, __LINE__, pc_ptr, env->opaque);
-#endif
         /* PARTIAL:
          * If the magic RAM location has been hit (either direct jump
          * or popping the relevant value off the stack with a 'ret'), we
@@ -1197,9 +1194,6 @@ static inline int gen_intermediate_code_internal(CPUState *env,
 
             if (magic && pc_ptr == magic)
             {
-#if 1	/* WmT - TRACE */
-;fprintf(stderr, "[%s:%d] PARTIAL - HANDLE TRAP HERE\n", __FILE__, __LINE__);
-;exit(1);
                 /* Equivalent ARM code calls its gen_exception() with
                  * EXCP_KERNEL_TRAP, and ends up calling a handler in
                  * linux-user/main.c
@@ -1207,7 +1201,14 @@ static inline int gen_intermediate_code_internal(CPUState *env,
                  * arguments], used to bail from disas_insn() in the
                  * failure case
                  */
-#endif
+//	/* in equivalent code, ARM emulation does:
+//	 * | gen_exception(EXCP_KERNEL_TRAP);
+//	 * | dc->is_jmp = DISAS_UPDATE;
+//	 * | break;
+//	 */
+                env->exception_index = EXCP_KERNEL_TRAP;
+                cpu_loop_exit(env);
+                break;
             }
         }
 #endif /* defined(CONFIG_USER_ONLY) && defined(TARGET_Z80) */
