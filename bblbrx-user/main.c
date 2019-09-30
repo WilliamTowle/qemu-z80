71ccc23fa56043a8cd819920a4997cc6503025b9
William Towle <william_towle@yahoo.co.uk>
2019-09-30 22:17:32 +0000

    bblbrx-user: add 'EXCP_KERNEL_TRAP' switch case

--- bblbrx-user/main.c
+++ bblbrx-user/main.c
@@ -118,10 +118,15 @@ void cpu_loop(CPUZ80State *env)
         trapnr= cpu_z80_exec(env);
         switch(trapnr)
         {
-        /* TODO: also clarify EXCP_KERNEL_TRAP ("clean exit") case.
-         * Presenting a register dump is informative here; for now,
-         * fall through to the "abnormal return" case
-         */
+        case EXCP_KERNEL_TRAP:
+            /* Our "magic ramtop" logic was triggered. In the
+             * simple code interpretation code case, we just exit.
+             * For a system with ROMs we might need to emulate or
+             * call relevant routines.
+             */
+            printf("Successful program exit. Register dump follows:\n");
+            cpu_dump_state(env, stderr, fprintf, 0);
+            break;	/* to exit() beyond switch */
         case EXCP06_ILLOP:
             printf("cpu_exec() encountered EXCP06_ILLOP (trapnr=%d) - aborting emulation\n", trapnr);
             break;	/* to exit() beyond switch */
