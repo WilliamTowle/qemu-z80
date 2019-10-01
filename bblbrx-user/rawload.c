02af1c035649a3ad7d93eb8594b577c693c3fcf9
William Towle <william_towle@yahoo.co.uk>
2019-10-01 20:03:55 +0000

    bblbrx-user: pread() file into RAM

--- bblbrx-user/rawload.c
+++ bblbrx-user/rawload.c
@@ -12,6 +12,7 @@
 #include "cpu.h"
 
 #include "qemu/error-report.h"
+#include "exec/cpu_ldst.h"
 
 
 #define EMIT_DEBUG 0
@@ -22,31 +23,20 @@
 int load_raw_binary(struct bblbrx_binprm *bprm)
 {
     abi_ulong   code_start, code_size;
+    ssize_t     read;
 
     code_start= 0x0000;
     code_size= bprm->filesize;
 
-#if 1	/* WmT - TRACE */
-;DPRINTF("%s(): PARTIAL - missing load (%u bytes from file %s) to guest addr 0x%04x\n", __func__, code_size, bprm->filename, code_start);
-#endif
-    /* PARTIAL: typical algorithm runs as follows
-     * 1. call load function; normally:
-    ...there are functions specific to file types
-    ...files are in sections, with size information encapsulated
-     * 2. determine code entry point and initialise stack/registers
-     * 3. return an appropriate status to the calling function
-     */
-    /* TODO:
-     * 1. memory model has:
-        ...code in low RAM addresses
-        ...stack growing downward from RAMTOP (64K)
-     * 2. file size tells us how many bytes to load
-     * 3. might want to set ROM/RAM split based on arguments
-     */
+    read= pread(bprm->fd, g2h(code_start), code_size, code_start);
+    if (read != code_size)
+    {
+        fprintf(stderr, "%s: %s\n", bprm->filename, strerror(errno));
+        exit(-ENOEXEC);
+    }
 
-#if 1
-;return fprintf(stderr, "%s(): PARTIAL - load routines incomplete\n", __func__);
-#else
-    return -ENOEXEC;
+#if 1   /* WmT - TRACE */
+;DPRINTF("%s(): pread() of %zd bytes successful\n", __func__, read);
 #endif
+    return 0;   /* "success" */
 }
