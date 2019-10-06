55dbd709039daef27caf266fab25582dba62b08d
William Towle <william_towle@yahoo.co.uk>
2019-10-06 14:11:03 +0000

    FIX: Make z80 gen_exception() TCG aware (QEmu v0.15.0)

--- target-z80/translate.c
+++ target-z80/translate.c
@@ -64,9 +64,14 @@ typedef struct DisasContext {
 static void gen_exception(DisasContext *s, int trapno, target_ulong cur_pc)
 {
     gen_jmp_im(cur_pc);
+#if 0	/* obsolete by 0.15.0 */
     gen_helper_raise_exception(trapno);
+#else	/* v0.15.0+ */
+    gen_helper_raise_exception(tcg_const_i32(trapno));
+#endif
     s->is_jmp = 3;
 }
+
 /* convert one instruction. s->is_jmp is set if the translation must
    be stopped. Return the next pc value */
 static target_ulong disas_insn(DisasContext *s, target_ulong pc_start)
