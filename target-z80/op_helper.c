f1aab6a0c5e6ce4804cd573ea70fce1422e28113
William Towle <william_towle@yahoo.co.uk>
2019-10-13 18:37:45 +0000

    z80 translate.c: parse/translate di/ei and rst

--- target-z80/op_helper.c
+++ target-z80/op_helper.c
@@ -297,6 +297,9 @@ void HELPER(cp_cc)(void)
 
 ///* Rotation/shift operations */
 
+
+///* Z80-specific: R800 has tst instruction */
+
 ///* Block instructions */
 
 /* misc */
@@ -397,6 +400,7 @@ void HELPER(ccf_cc)(void)
     F = (F & (CC_S | CC_Z | CC_P)) | hf | cf;
 }
 
+
 /* word operations -- HL only? */
 
 void HELPER(sbcw_T0_T1_cc)(void)
@@ -484,4 +488,17 @@ void HELPER(decb_T0_cc)(void)
 
     F = (F & CC_C) | sf | zf | hf | CC_N | pf;
     /* TODO: check CC_N is set */
+}
+/* enable interrupts */
+void HELPER(ei)(void)
+{
+    env->iff1 = 1;
+    env->iff2 = 1;
+}
+
+/* disable interrupts */
+void HELPER(di)(void)
+{
+    env->iff1 = 0;
+    env->iff2 = 0;
 }
