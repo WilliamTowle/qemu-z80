6ea482b39fea7b62cb0c61453590104c514c6935
William Towle <william_towle@yahoo.co.uk>
2019-10-13 15:05:27 +0000

    z80 translate.c: arithmetic parse/helpers

--- target-z80/helper.h
+++ target-z80/helper.h
@@ -16,4 +16,27 @@ DEF_HELPER_1(movl_pc_im, void, i32)
 DEF_HELPER_0(jmp_T0, void)
 //DEF_HELPER_2(djnz, void, i32, i32)
 
+/* 8-bit arithmetic */
+DEF_HELPER_0(add_cc, void)
+DEF_HELPER_0(adc_cc, void)
+DEF_HELPER_0(sub_cc, void)
+DEF_HELPER_0(sbc_cc, void)
+DEF_HELPER_0(and_cc, void)
+DEF_HELPER_0(xor_cc, void)
+DEF_HELPER_0(or_cc, void)
+DEF_HELPER_0(cp_cc, void)
+
+///* Rotation/shifts */
+
+///* Block instructions */
+
+///* Misc */
+
+/* 16-bit arithmetic */
+DEF_HELPER_0(sbcw_T0_T1_cc, void)
+DEF_HELPER_0(addw_T0_T1_cc, void)
+DEF_HELPER_0(adcw_T0_T1_cc, void)
+DEF_HELPER_0(incb_T0_cc, void)
+DEF_HELPER_0(decb_T0_cc, void)
+
 #include "def-helper.h"
