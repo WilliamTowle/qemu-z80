6bba26ca63c0207b0909e781c6390a7aa006a564
William Towle <william_towle@yahoo.co.uk>
2019-12-26 09:56:28 +0000

    z80: Annotate gen_intermediate_code_internal() entry/exit

--- target-z80/translate.c
+++ target-z80/translate.c
@@ -1074,6 +1074,9 @@ static inline void gen_intermediate_code_internal(Z80CPU *cpu,
     target_ulong cs_base;
 //    int num_insns;
 //    int max_insns;
+#if 1	/* WmT - TRACE */
+;DPRINTF("*** ENTER %s() ****\n", __func__);
+#endif
 
 //    /* generate intermediate code */
     pc_start = tb->pc;
@@ -1203,6 +1206,9 @@ static inline void gen_intermediate_code_internal(Z80CPU *cpu,
 ;fprintf(stderr, "** BAILING - %s() PARTIAL IMPLEMENTATION ONLY **\n", __func__);
 ;exit(1);
 #endif
+#endif
+#if 1	/* WmT - TRACE */
+;DPRINTF("*** EXIT %s(), OK ***\n", __func__);
 #endif
 }
 
