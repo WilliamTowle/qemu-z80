b50ac81740662ace04fb30254f61231fa448fc94
William Towle <william_towle@yahoo.co.uk>
2019-12-27 15:53:51 +0000

    z80 codegen: op_helper.c IRQ inhibit

--- target-z80/helper.h
+++ target-z80/helper.h
@@ -3,7 +3,7 @@
 DEF_HELPER_1(raise_exception, void, i32)
 
 //DEF_HELPER_0(set_inhibit_irq, void)
-//DEF_HELPER_0(reset_inhibit_irq, void)
+DEF_HELPER_0(reset_inhibit_irq, void)
 
 DEF_HELPER_1(movl_pc_im, void, i32)
 
