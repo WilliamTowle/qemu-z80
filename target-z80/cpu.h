a59f3326e27a91529fbc5f78da4f96c2e6a156fb
William Towle <william_towle@yahoo.co.uk>
2020-05-20 18:46:37 +0000

    z80-softmmu: cpu.h needs cpu_mmu_index()

--- target-z80/cpu.h
+++ target-z80/cpu.h
@@ -173,7 +173,16 @@ int cpu_z80_handle_mmu_fault(CPUZ80State *env, target_ulong addr,
 #define cpu_exec cpu_z80_exec
 #define cpu_signal_handler cpu_z80_signal_handler
 
+
 #define MMU_USER_IDX 1
+
+static inline int cpu_mmu_index(CPUState *env)
+{
+    /* see i386? returns 1 or 0 depending on HF_CPL_MASK setting:
+     *	return (env->hflags & HF_CPL_MASK) == 3 ? 1 : 0;
+     */
+    return 0;
+}
 
 #include "cpu-all.h"
 
