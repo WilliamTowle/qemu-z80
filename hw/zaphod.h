321af810a82687298565d3ab1df87f6ac2847eb2
William Towle <william_towle@yahoo.co.uk>
2020-05-26 20:07:44 +0000

    Zaphod for v0.15.0: stub functions for monitor

--- hw/zaphod.h
+++ hw/zaphod.h
@@ -6,12 +6,20 @@
 
 #define ZAPHOD_DEBUG	1	/* WARNING: uses fprintf() */
 
+#include "hw/boards.h"		/* emulator datatypes */
+
+
 /* "Zaphod" Z80 machine family configuration */
 
 /* ZAPHOD_MAX_RAMTOP:
  * Address space for a Z80 ends at 64K (some emulations might want less)
  */
 #define	ZAPHOD_MAX_RAMTOP	(64 * 1024)
+
+/* zaphod.c content */
+
+void pic_info(Monitor *mon);
+void irq_info(Monitor *mon);
 
 
 #endif	/*  _ZAPHOD_H_  */
