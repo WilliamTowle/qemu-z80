9c75b5afc5a789ae0416ee6a0fe90a021b97fbc1
William Towle <william_towle@yahoo.co.uk>
2020-05-31 09:36:01 +0000

    WIP: rework RAM size setup/references

--- hw/zaphod.c
+++ hw/zaphod.c
@@ -15,10 +15,145 @@
 #include "hw/loader.h"
 
 
+/* Machine configuration */
+#define ZAPHOD_RAM_SIZE		ZAPHOD_MAX_RAMTOP
+
 typedef struct {
 	CPUState		*cpu;
 	ram_addr_t		ram_handle;
 } ZaphodState;
+
+
+/* pic_info() and irq_info() are monitor functions for
+ * hardware we don't have
+ */
+
+void pic_info(Monitor *mon)
+{
+}
+
+void irq_info(Monitor *mon)
+{
+}
+
+/*  Zaphod-1 has a "medium-res two-colour 24x80 screen" and its
+ *  console responds with "An IN from port 0 will respond with the
+ *  ASCII code of the last key pressed, or 0 if none available. An
+ *  OUT to port 1 will display the appropriate character on the
+ *  console screen". The documented intention is to have the screen
+ *  support VT52 escape sequences, but for temporary:
+ *	ESC 0     - clears the console screen
+ *	ESC 1 x y - moves the cursor position to x,y
+ *	ESC 2     - clears to end of line from the current position
+ */
+
+#ifdef ZAPHOD_HAS_MACHINESPEC
+#define	ZAPHOD_SPEC_DEVEL	0	/* misc config */
+#define ZAPHOD_SPEC_ZAPHODPB	1	/* Phil Brown */
+#endif
+
+#ifdef ZAPHOD_HAS_IOPORTS
+static uint32_t zaphod_io_read(void *opaque, uint32_t addr)
+{
+  ZaphodState	*zs= (ZaphodState *)opaque;
+  int		value;
+//#ifdef ZAPHOD_DEBUG
+//	fprintf(stderr, "DEBUG: Enter %s()\n", __func__);
+////;fprintf(stderr, "*** IN (%s) with inkey value %d ***\n", __func__, value);
+//#endif
+	switch (addr)
+	{
+	case 0x00:		/* Zaphod: STDIN */
+		value= zs->console->cs_inkey;
+		zs->console->cs_inkey= 0;
+#ifdef ZAPHOD_HAS_RXINT_IRQ
+		if (zs->console->rxint_irq)
+		    qemu_irq_lower(*zs->console->rxint_irq);
+#endif	/* ZAPHOD_HAS_RXINT_IRQ */
+		return value;
+#ifdef ZAPHOD_HAS_SERIALIO
+	case 0x80:		/* read mc6850 PortStatus */
+		value= (zs->console->cs_inkey)? 0x01 : 0; /* RxDataReady */
+		value|= 0x02;		/* TxDataEmpty (always) */
+		value|= 0x04;		/* DTD [Data Carrier Detect] */
+		value|= 0x08;		/* CTS [Clear to Send] */
+			/* FrameErr|Overrun|ParityErr|IrqReq not emulated */
+#ifdef ZAPHOD_DEBUG
+		//fprintf(stderr, "DEBUG: %s() read mc6850 PortStatus (port 0x%02x) -> status %02x\n", __func__, addr, value);
+#endif
+		return value;
+	case 0x81:		/* read mc6850 RxData */
+		value= zs->console->cs_inkey;
+		zs->console->cs_inkey= 0;
+#ifdef ZAPHOD_HAS_RXINT_IRQ
+		if (zs->console->rxint_irq)
+			qemu_irq_lower(*zs->console->rxint_irq);
+#endif	/* ZAPHOD_HAS_RXINT_IRQ */
+#ifdef ZAPHOD_DEBUG
+		//fprintf(stderr, "DEBUG: %s() read mc6850 RXData (port 0x%02x) -> ch-value %d\n", __func__, addr, value);
+#endif
+		//return value;
+		return value? value : 0xff;
+#endif	/* ZAPHOD_HAS_SERIALIO */
+	default:
+#ifdef ZAPHOD_DEBUG
+		//fprintf(stderr, "DEBUG: %s() Unexpected read, with port=%d\n", __func__, addr);
+#endif
+		return 0x00;
+	}
+//#ifdef ZAPHOD_DEBUG
+//	fprintf(stderr, "DEBUG: Exit %s()\n", __func__);
+//#endif
+}
+
+static void zaphod_io_write(void *opaque, uint32_t addr, uint32_t value)
+{
+  ZaphodState	*zs= (ZaphodState *)opaque;
+
+	switch (addr)
+	{
+	case 0x01:		/* Zaphod: STDOUT */
+#ifdef ZAPHOD_HAS_CONSOLEGUI
+	/* TODO: how to manage/display text in non-GUI mode? */
+#ifdef ZAPHOD_DEBUG
+;fprintf(stderr, "%s: character value: %d\n", __func__, value);
+#endif
+#ifdef ZAPHOD_CONSOLE_PSEUDO_CURPOS /* forcibly show capitals */
+	    zaphod_consolegui_putchar(zs->console, toupper(value & 0xff));
+#else
+	    zaphod_consolegui_putchar(zs->console, value & 0xff);
+#endif
+#endif	/* ZAPHOD_HAS_CONSOLEGUI */
+#ifdef ZAPHOD_HAS_SERIALIO
+	    zaphod_serio_putchar(/*zs->console, */value & 0xff);
+#endif
+	    break;
+#ifdef ZAPHOD_HAS_SERIALIO
+	case 0x80:		/* write -> mc6850 PortControl */
+		/* ignore since baud rate change etc. not emulated? */
+#ifdef ZAPHOD_DEBUG
+		fprintf(stderr, "DEBUG: %s() write m6850 PortControl (port 0x%02x) <- value %d\n", __func__, addr, value);
+#endif
+		break;
+	case 0x81:		/* write -> mc6850 TxData */
+#ifdef ZAPHOD_DEBUG
+		//fprintf(stderr, "DEBUG: %s() write mc6850 TxData (port 0x%02x) -> ch-value=%d\n", __func__, addr, value);
+#endif
+#ifdef ZAPHOD_HAS_CONSOLEGUI
+		zaphod_consolegui_putchar(zs->console, toupper(value & 0xff));
+#endif
+		zaphod_serio_putchar(/*zs->console, */value & 0xff);
+		break;
+#endif	/* ZAPHOD_HAS_SERIALIO */
+	default:
+#ifdef ZAPHOD_DEBUG
+		fprintf(stderr, "DEBUG: %s() Unexpected write, port 0x%02x, value %d\n", __func__, addr, value);
+#endif
+	    break;
+	}
+}
+#endif	/* ZAPHOD_HAS_IOPORTS */
+
 
 static CPUState* zaphod_new_cpu(const char *cpu_model)
 {
