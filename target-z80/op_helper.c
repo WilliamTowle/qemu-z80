2dc69bb8c6a79136410a896784d521bccd11a8cd
William Towle <william_towle@yahoo.co.uk>
2020-05-31 16:25:51 +0000

    FIX: op_helper.c adjust/build cpu_{in|out}b()
    Based on Mar 25, 2011 commit at "dirty hack to fix SIGSEGV when
    doing IN, OUT and A is not zero."
    
    With additional #include and notes on ideal datatypes
    
    https://github.com/legumbre/qemu-z80/commit/b14134a2

--- target-z80/op_helper.c
+++ target-z80/op_helper.c
@@ -24,6 +24,8 @@
 #include "helper.h"
 
 
+#include "ioport.h"
+
 #if 1  /* was: TARGET_LONG_BITS > HOST_LONG_BITS
         * but TCG_AREG{1|2} no longer available for else case
         */
@@ -206,14 +208,17 @@ void HELPER(halt)(CPUZ80State *env)
 
 void HELPER(in_T0_im)(CPUZ80State *env, uint32_t val)
 {
-    T0 = cpu_inb(env, (A << 8) | val);
+    ////    T0 = cpu_inb(env, (A << 8) | val);
+    //T0 = cpu_inb(env, val);
+    T0 = cpu_inb(val);	/* TODO: 'val' should have type pio_addr_t */
 }
 
 void HELPER(in_T0_bc_cc)(CPUZ80State *env)
 {
     int sf, zf, pf;
 
-    T0 = cpu_inb(env, BC);
+    //T0 = cpu_inb(env, BC);
+    T0 = cpu_inb(BC);	/* TODO: 'BC' should have type pio_addr_t */
 
     sf = (T0 & 0x80) ? CC_S : 0;
     zf = T0 ? 0 : CC_Z;
@@ -223,12 +228,15 @@ void HELPER(in_T0_bc_cc)(CPUZ80State *env)
 
 void HELPER(out_T0_im)(CPUZ80State *env, uint32_t val)
 {
-    cpu_outb(env, (A << 8) | val, T0);
+    //// cpu_outb(env, (A << 8) | val, T0);
+    //cpu_outb(env, val, T0);
+    cpu_outb(val, T0);		/* TODO: 'val' should have type pio_addr_t */
 }
 
 void HELPER(out_T0_bc)(CPUZ80State *env)
 {
-    cpu_outb(env, BC, T0);
+    //cpu_outb(env, BC, T0);
+    cpu_outb(BC, T0);		/* TODO: 'BC' should have type pio_addr_t */
 }
 
 /* Misc */
