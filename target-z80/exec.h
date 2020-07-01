03bf8362c0013f5552b6bfabec931c4118951a87
William Towle <william_towle@yahoo.co.uk>
2020-07-01 21:54:17 +0000

    z80-softmmu: op_helper.c do_interrupt()

--- target-z80/exec.h
+++ target-z80/exec.h
@@ -47,6 +47,8 @@ register struct CPUZ80State *env asm(AREG0);
 
 //#define PC  (env->pc)
 
+void do_interrupt(CPUZ80State *env);
+
 /* op_helper.c */
 void raise_interrupt(int intno, int is_int, int error_code,
                      int next_eip_addend);
