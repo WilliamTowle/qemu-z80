d83f04142ed5f7abad66bf382f9fdd0bf639b373
William Towle <william_towle@yahoo.co.uk>
2020-10-17 17:30:43 +0000

    target-z80: cpu.c CPU skeleton

---
+++ target-z80/cpu.c
@@ -0,0 +1,11 @@
+/*
+ * Skeleton for minimal QEmu Z80 CPU
+ * vim: ft=c sw=4 ts=4 et :
+ *
+ * WmT, 2018-2022 [...after Stuart Brady, under GPL...]
+ */
+
+//#include "qemu/error-report.h"
+//
+//#define DPRINTF(fmt, ...) \
+//    do { error_printf( "Z80 cpu.c: " fmt , ## __VA_ARGS__); } while(0)
