22afc1fd08e8b09094ed0a361fed1958d8e857fe
William Towle <william_towle@yahoo.co.uk>
2020-10-17 17:30:43 +0000

    target/z80: CPU skeleton - headers, code files

---
+++ target/z80/cpu-param.h
@@ -0,0 +1,13 @@
+/*
+ * Minimal QEmu Z80 CPU
+ * vim: ft=c sw=4 ts=4 et :
+ *
+ *  Copyright (c) 2018-2022 William Towle <william_towle@yahoo.co.uk>
+ */
+
+#ifndef Z80_CPU_PARAM_H
+#define Z80_CPU_PARAM_H
+
+/* TODO: TARGET_LONG_BITS, TARGET_PAGE_BITS, NB_MMU_MODES */
+
+#endif /* !defined Z80_CPU_PARAM_H */
---
+++ target/z80/misc_helper.c
@@ -0,0 +1,7 @@
+/*
+ * Minimal QEmu Z80 CPU - misc helpers
+ * vim: ft=c sw=4 ts=4 et :
+ *
+ *  Ported/provisional reimplementations by William Towle 2018-2022
+ *  All versions under GPL
+ */
