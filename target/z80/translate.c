110d47abb441d8e0c3f8d0d38604abc4b596262e
William Towle <william_towle@yahoo.co.uk>
2020-10-17 23:06:43 +0000

    target/z80: gen_intermediate_code() with "partial" bail

--- target/z80/translate.c
+++ target/z80/translate.c
@@ -21,6 +21,18 @@
     do { if (EMIT_DEBUG) error_printf("Z80 translate: " fmt , ## __VA_ARGS__); } while(0)
 
 
+/* TODO: struct DisasContext, translation ops/disas_insn() */
+
+
+/* generate intermediate code for basic block 'tb'.  */
+void gen_intermediate_code(CPUState *cs, TranslationBlock *tb, int max_insns)
+{
+#if 1   /* WmT - PARTIAL */
+;DPRINTF("Z80 translate.c is incomplete (%s() PARTIAL)\n", __func__);
+;exit(1);
+#endif
+}
+
 void restore_state_to_opc(CPUZ80State *env, TranslationBlock *tb,
                             target_ulong *data)
 {
