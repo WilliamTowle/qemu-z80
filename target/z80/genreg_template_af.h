848bf29d175c500ad01271591a715d4d89e0634c
William Towle <william_towle@yahoo.co.uk>
2020-10-17 17:30:43 +0000

    target/z80: other headers, code files

---
+++ target/z80/genreg_template_af.h
@@ -0,0 +1,7 @@
+/*
+ * QEMU Z80 CPU
+ * Register templates for Z80 CPU
+ * vim: ft=c sw=4 ts=4 et :
+ *
+ *  Copyright (c) 2007-2009 Stuart Brady <stuart.brady@gmail.com>
+ */
---
+++ target/z80/helper.c
@@ -0,0 +1,8 @@
+/*
+ * Minimal QEmu Z80 CPU - helpers
+ * vim: ft=c sw=4 ts=4 et :
+ *
+ *  Copyright (c) 2007-2009 Stuart Brady <stuart.brady@gmail.com>
+ *  Ported/provisional reimplementations by William Towle 2018-2022
+ *  All versions under GPL
+ */
