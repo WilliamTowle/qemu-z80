2887eecf696dd093e46c67dca2a33cd37dd6e947
William Towle <william_towle@yahoo.co.uk>
2020-10-18 11:56:53 +0000

    bblbrx: add {lock|unlock}_user() [QEmu v2]
    exec.c states that cpu_memory_rw_debug() under CONFIG_USER_ONLY
    "should not depend on lock_user() [here]".
    
    Provide functions to keep the build happy since it clearly does.

--- bblbrx-user/qemu.h
+++ bblbrx-user/qemu.h
@@ -10,6 +10,60 @@
 #define QEMU_H
 
 
+#include "cpu.h"
+#include "exec/cpu_ldst.h"
+
+
+/* QEmu user access */
+
+#define VERIFY_READ 0
+#define VERIFY_WRITE 1 /* implies read access */
+
+static inline int access_ok(int type, abi_ulong addr, abi_ulong size)
+{
+    return page_check_range((target_ulong)addr, size,
+                            (type == VERIFY_READ) ? PAGE_READ : (PAGE_READ | PAGE_WRITE)) == 0;
+}
+
+/* Lock an area of guest memory into the host.  If copy is true then the
+   host area will have the same contents as the guest.  */
+static inline void *lock_user(int type, abi_ulong guest_addr, long len, int copy)
+{
+    if (!access_ok(type, guest_addr, len))
+        return NULL;
+#ifdef DEBUG_REMAP
+    {
+        void *addr;
+        addr = g_malloc(len);
+        if (copy)
+            memcpy(addr, g2h(guest_addr), len);
+        else
+            memset(addr, 0, len);
+        return addr;
+    }
+#else
+    return g2h(guest_addr);
+#endif
+}
+
+/* Unlock an area of guest memory.  The first LEN bytes must be
+   flushed back to guest memory. host_ptr = NULL is explicitly
+   allowed and does nothing. */
+static inline void unlock_user(void *host_ptr, abi_ulong guest_addr,
+                               long len)
+{
+#ifdef DEBUG_REMAP
+    if (!host_ptr)
+        return;
+    if (host_ptr == g2h(guest_addr))
+        return;
+    if (len > 0)
+        memcpy(g2h(guest_addr), host_ptr, len);
+    g_free(host_ptr);
+#endif
+}
+
+
 struct bblbrx_binprm {
     const char      *filename;
     int             fd;
