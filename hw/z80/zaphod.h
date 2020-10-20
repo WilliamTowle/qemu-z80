5afd3bb9ec8677735620594e7e62942fc407d6f5
William Towle <william_towle@yahoo.co.uk>
2020-10-20 13:38:10 +0000

    Initial zaphod.[ch]

---
+++ hw/z80/zaphod.h
@@ -0,0 +1,20 @@
+/*
+ * QEmu Zaphod sample board
+ * vim: ft=c sw=4 ts=4 et :
+ *
+ * [...William Towle c. 2013-2022, under GPL...]
+ */
+
+#ifndef HW_Z80_ZAPHOD_H
+#define HW_Z80_ZAPHOD_H
+
+
+#define ZAPHOD_DEBUG    1
+
+/* TODO: Config-related defines - see also z80-softmmu.mak */
+
+/* TODO: Z80_MAX_RAM_SIZE and related defines */
+
+/* TODO: ZaphodMachineClass, ZaphodMachineState types here */
+
+#endif  /*  HW_Z80_ZAPHOD_H  */
