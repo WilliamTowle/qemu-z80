b2ce929bb69fb93a167309714233012be7bbcee9
William Towle <william_towle@yahoo.co.uk>
2020-10-20 22:15:40 +0000

    disas/z80.c w/ build

---
+++ disas/z80.c
@@ -0,0 +1,16 @@
+/*
+ * QEMU Z80
+ * Instruction printing code for the Z80 instruction set
+ * vim: ft=c sw=4 ts=4 et :
+ *
+ * [...Stuart Brady/William Towle, under GPL...]
+ */
+
+#include "qemu/osdep.h"
+#include "disas/dis-asm.h"
+
+int
+print_insn_z80(bfd_vma pc, struct disassemble_info *info)
+{
+#error "..."
+}
