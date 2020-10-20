d4c4f8dc21b443d29d059087c32f719bac1e3d1c
William Towle <william_towle@yahoo.co.uk>
2020-10-20 22:31:38 +0000

    disas: bytewise "Z80" disassembly configure/build

--- disas/z80.c
+++ disas/z80.c
@@ -9,8 +9,22 @@
 #include "qemu/osdep.h"
 #include "disas/dis-asm.h"
 
+
 int
-print_insn_z80(bfd_vma pc, struct disassemble_info *info)
+print_insn_z80(bfd_vma memaddr, struct disassemble_info *info)
 {
-#error "..."
+    fprintf_function fprintf_fn = info->fprintf_func;
+    int status;
+    unsigned char byte;
+    void *stream = info->stream;
+
+    status= info->read_memory_func(memaddr, &byte, sizeof byte, info);
+    if (status != 0)
+    {
+        info->memory_error_func(status, memaddr, info);
+        return -1;
+    }
+
+    fprintf_fn (stream, ".byte 0x%02x", byte);
+    return 1;
 }
