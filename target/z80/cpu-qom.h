8fa43d36b9df307074c46063e5751de643427233
William Towle <william_towle@yahoo.co.uk>
2020-10-20 19:23:55 +0000

    target/z80: cpu-qom.h Z80CPUClass

--- target/z80/cpu-qom.h
+++ target/z80/cpu-qom.h
@@ -13,8 +13,26 @@
 
 #define TYPE_Z80_CPU "z80-cpu"
 
-typedef struct Z80CPU Z80CPU;
+#define Z80_CPU_CLASS(class) \
+    OBJECT_CLASS_CHECK(Z80CPUClass, (class), TYPE_Z80_CPU)
+#define Z80_CPU(obj) \
+    OBJECT_CHECK(Z80CPU, (obj), TYPE_Z80_CPU)
+#define Z80_CPU_GET_CLASS(obj) \
+    OBJECT_GET_CLASS(Z80CPUClass, (obj), TYPE_Z80_CPU)
 
-/* TODO: Z80CPUClass */
+/**
+ *  Z80CPUClass
+ */
+typedef struct Z80CPUClass {
+    /*< private >*/
+    CPUClass parent_class;
+
+    /*< public >*/
+    DeviceRealize parent_realize;
+    DeviceUnrealize parent_unrealize;
+    void (*parent_reset)(CPUState *cpu);
+} Z80CPUClass;
+
+typedef struct Z80CPU Z80CPU;
 
 #endif /* !QEMU_Z80_CPU_QOM_H */
