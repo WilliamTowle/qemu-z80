6c43877a1645c4a8f15873921bbdb526ee8186ae
William Towle <william_towle@yahoo.co.uk>
2020-10-27 17:23:57 +0000

    target-z80: Z80CPU/ENV_GET_CPU (via initial cpu-qom.h)

--- target-z80/cpu-qom.h
+++ target-z80/cpu-qom.h
@@ -8,8 +8,33 @@
 #ifndef QEMU_Z80_QOM_H
 #define QEMU_Z80_QOM_H
 
+#include "qom/cpu.h"
+#include "cpu.h"
+//#include "qapi/error.h"
 
-/* TODO: Z80CPU, ENV_GET_CPU() */
 
+/**
+ * Z80CPU:
+ * @env: #CPUZ80State
+ *
+ * A z80 CPU.
+ */
+typedef struct Z80CPU {
+    /*< private >*/
+    CPUState parent_obj;
+
+    /*< public >*/
+    CPUZ80State env;
+} Z80CPU;
+
+static inline Z80CPU *z80_env_get_cpu(CPUZ80State *env)
+{
+    return container_of(env, Z80CPU, env);
+}
+
+
+#define ENV_GET_CPU(e) CPU(z80_env_get_cpu(e))
+
+#define ENV_OFFSET offsetof(Z80CPU, env)
 
 #endif /* !defined (QEMU_Z80_CPU_QOM_H) */
