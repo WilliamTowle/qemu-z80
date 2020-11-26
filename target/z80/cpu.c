3731f5c6d6710b35850a021d2627dfca221885a0
William Towle <william_towle@yahoo.co.uk>
2020-11-26 21:57:21 +0000

    excp_helper: add minimal z80_cpu_tlb_fill()

--- target/z80/cpu.c
+++ target/z80/cpu.c
@@ -286,7 +286,12 @@ static void z80_cpu_class_init(ObjectClass *oc, void *data)
 //    cc->gdb_write_register = z80_cpu_gdb_write_register;
 //    cc->gdb_num_core_regs = 35;	/* NUMBER_OF_CPU_REGISTERS? */
 //    cc->gdb_core_xml_file = "z80-cpu.xml";
-#if !defined(CONFIG_USER_ONLY)
+#ifdef CONFIG_USER_ONLY
+    /* Without an MMU fault handler, QEmu v2's accel/tcg/user-exec.c
+     * asserts. Like lm32, ours just wraps tlb_set_page()
+     */
+    //cc->handle_mmu_fault = z80_cpu_handle_mmu_fault;
+#else	/* !defined(CONFIG_USER_ONLY) */
     cc->get_phys_page_debug = z80_cpu_get_phys_page_debug;
 #endif
 //    cc->vmsd = &vmstate_z80_cpu;
