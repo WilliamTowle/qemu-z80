f8cdf27fe32bf4028834f5bec9a3706a149b1d13
William Towle <william_towle@yahoo.co.uk>
2020-12-04 17:12:03 +0000

    target/z80: interrupt/exception helpers and state

--- target/z80/excp_helper.c
+++ target/z80/excp_helper.c
@@ -6,10 +6,43 @@
  *  All versions under GPL
  */
 
+#include "qemu/osdep.h"
+#include "cpu.h"
 
 #include "qemu/error-report.h"
+#include "exec/exec-all.h"
+#include "exec/helper-proto.h"
+
 
 //#define EMIT_DEBUG ZAPHOD_DEBUG
 #define EMIT_DEBUG 0
 #define DPRINTF(fmt, ...) \
     do { if (EMIT_DEBUG) error_printf("Z80 excp_helper: " fmt , ## __VA_ARGS__); } while(0)
+
+
+/*
+ * Signal an interruption. It is executed in the main CPU loop.
+ * is_int is TRUE if coming from the int instruction. next_eip is the
+ * env->eip value AFTER the interrupt instruction. It is only relevant if
+ * is_int is TRUE.
+ */
+static void QEMU_NORETURN raise_interrupt2(CPUZ80State *env, int intno,
+                                           int is_int, int error_code)
+{
+    CPUState *cs = CPU(z80_env_get_cpu(env));
+
+    cs->exception_index = intno;
+    env->error_code = error_code;
+    env->exception_is_int = is_int;
+    cpu_loop_exit(cs);
+}
+
+void raise_exception(CPUZ80State *env, int exception_index)
+{
+    raise_interrupt2(env, exception_index, 0, 0);
+}
+
+void helper_raise_exception(CPUZ80State *env, int exception_index)
+{
+    raise_exception(env, exception_index);
+}
