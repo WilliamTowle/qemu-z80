ce0028a8c1ff1a7917db0d919956d74c242dfc0f
William Towle <william_towle@yahoo.co.uk>
2020-12-06 14:06:09 +0000

    bblbrx-user: QEmuify basic skeleton [for v5]
    Expand usage message and add skeleton for bblbrx_exec()

--- bblbrx-user/main.c
+++ bblbrx-user/main.c
@@ -6,11 +6,11 @@
  *  [...under GPL...]
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-//#include "qemu/error-report.h"
+#include "qemu/osdep.h"
+#include "qemu.h"
+
+#include "qemu/error-report.h"
 
 //#define EMIT_DEBUG ZAPHOD_DEBUG
 #define EMIT_DEBUG 0
@@ -21,9 +21,7 @@
 static void usage(int exitcode)
 {
     /* NB: platforms may pass program arguments */
-    printf(//"Usage: qemu-" TARGET_NAME " [options] program [arguments...]\n"
-        "Usage: bblbrx-main [options] program\n"
-        );
+    printf("Usage: qemu-" TARGET_NAME " [options] program\n");
     exit(exitcode);
 }
 
@@ -49,6 +47,8 @@ static int parse_args(int argc, char **argv)
         {
             usage(EXIT_SUCCESS);
         }
+        /* TODO: support "cpu" option */
+        /* TODO: support "singlestep" option */
         else
         {
             fprintf(stderr, "Unexpected option '%s'\n", &r[1]);
@@ -63,15 +63,49 @@ int main(int argc, char **argv)
 {
     char *filename;
     int optind;
+    int ret;
 
     if (argc <= 1)
         usage(EXIT_SUCCESS);    /* effectively "--help" */
+
+    /* PARTIAL: prior to argument parsing, v5 has:
+     * 1. error_init() is called, and the TRACE subsystem set up
+     * 2. qemu_init_cpu_list() is called, and QOM subsystem set up
+     * 3. target environment and stack get set up, if required
+     * 4. cpu_model is defaulted to NULL
+     * 5. trace options and plugins are managed
+     */
 
     optind= parse_args(argc, argv);
     if (optind >= argc)
         usage(EXIT_FAILURE);
     filename= argv[optind];
 
-    fprintf(stderr, "%s: Filename '%s' was given...\n", argv[0], filename);
-    exit(EXIT_FAILURE);
+    /* PARTIAL:
+     * to effect CPU init, following parse_cpu_option() we:
+     *  1. call tcg_exec_init()
+     *  2. initialise 'cpu' from cpu_create() result
+     *  3. calls cpu_reset()
+     *  4. initialise 'thread_cpu'
+     *  5. handle CONFIG_USE_GUEST_BASE/target_ram and TCG init
+     *  6. manage passing arg{c|v} to target, if required
+     *  7. allocate/initialise any TaskState
+     */
+
+    ret= bblbrx_exec(filename);
+    if (ret != 0) {
+        if (ret > 0)
+            printf("%s(): BAILING - unexpected bblbrx_exec() retval %d while loading %s\n", __func__, ret, filename);
+        else
+            printf("Error while loading %s: %s\n", filename, strerror(-ret));
+        exit(EXIT_FAILURE);
+    }
+
+#if 1   /* WmT - PARTIAL */
+;return fprintf(stderr, "%s(): INCOMPLETE - need cpu_loop() to execute %s\n", __func__, filename);
+#else
+    /* NB: cpu_loop() exits on ILLOP/KERNEL_TRAP */
+
+  return EXIT_SUCCESS;
+#endif
 }
