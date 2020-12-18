0d0855a3b90b31c8767474b02eb0f16f12cc8f1d
William Towle <william_towle@yahoo.co.uk>
2020-12-18 18:08:40 +0000

    WIP: add 8-bit register load/store

--- target/z80/genreg_template.h
+++ target/z80/genreg_template.h
@@ -6,6 +6,23 @@
  * [...Stuart Brady, under GPL...]
  */
 
+/* 8-bit Loads */
+
+static inline void glue(gen_movb_v_, REGHIGH)(TCGv v)
+{
+	tcg_gen_ld8u_tl(v, cpu_env,
+                    offsetof(CPUZ80State, regs[glue(R_,REGPAIR)]) +
+                                            BYTE_OFFSET(cpu_env->regs[], 1));
+}
+
+static inline void glue(gen_movb_v_, REGLOW)(TCGv v)
+{
+	tcg_gen_ld8u_tl(v, cpu_env,
+			offsetof(CPUZ80State, regs[glue(R_,REGPAIR)]) +
+                                            BYTE_OFFSET(cpu_env->regs[], 0));
+}
+
+
 /* 16-bit loads */
 
 static inline void glue(gen_movw_v_, REGPAIR)(TCGv v)
@@ -13,6 +30,23 @@ static inline void glue(gen_movw_v_, REGPAIR)(TCGv v)
     tcg_gen_ld16u_tl(v, cpu_env,
                      offsetof(CPUZ80State, regs[glue(R_,REGPAIR)]) +
                                              WORD_OFFSET(cpu_env->regs[], 0));
+}
+
+
+/* 8-bit stores */
+
+static inline void glue(glue(gen_movb_,REGHIGH),_v)(TCGv v)
+{
+    tcg_gen_st8_tl(v, cpu_env,
+                   offsetof(CPUZ80State, regs[glue(R_,REGPAIR)]) +
+                                           BYTE_OFFSET(cpu_env->regs[], 1));
+}
+
+static inline void glue(glue(gen_movb_,REGLOW),_v)(TCGv v)
+{
+    tcg_gen_st8_tl(v, cpu_env,
+                   offsetof(CPUZ80State, regs[glue(R_,REGPAIR)]) +
+                                           BYTE_OFFSET(cpu_env->regs[], 0));
 }
 
 /* 16-bit stores */
