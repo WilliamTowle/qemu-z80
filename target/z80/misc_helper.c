d142406f74060df06edff2c1679394b63c5eb86b
William Towle <william_towle@yahoo.co.uk>
2020-12-22 22:45:44 +0000

    WIP: initial misc_helper.c - halt helper

--- target/z80/misc_helper.c
+++ target/z80/misc_helper.c
@@ -11,6 +11,24 @@
 #include "exec/exec-all.h"
 
 
+void helper_halt(CPUZ80State *env)
+{
+    Z80CPU *cpu = z80_env_get_cpu(env);
+    CPUState *cs = CPU(cpu);
+    //printf("halting at PC 0x%x\n",env->pc);
+
+    cs->halted = 1;
+    env->hflags &= ~HF_INHIBIT_IRQ_MASK; /* needed if sti is just before */
+    cs->exception_index = EXCP_HLT;
+#if 0  /* obsolete */
+    cpu_loop_exit();
+#else  /* v0.15.0+ */
+    //cpu_loop_exit(env);
+    cpu_loop_exit(cs);
+#endif
+}
+
+
 void helper_debug(CPUZ80State *env)
 {
     CPUState *cs = CPU(z80_env_get_cpu(env));
