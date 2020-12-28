567c2de5ce048db48ac09b2c8232d2ea2eb38f6b
William Towle <william_towle@yahoo.co.uk>
2020-12-28 19:10:46 +0000

    WIP: add main registers to cpu_dump_state() output

--- target/z80/helper.c
+++ target/z80/helper.c
@@ -19,13 +19,31 @@ void z80_cpu_dump_state(CPUState *cs, FILE *f, fprintf_function cpu_fprintf,
     CPUZ80State *env= &cpu->env;
 
 #if 1   /* PARTIAL */
-    cpu_fprintf(f, "[PARTIAL - AF/BC/DE/HL/IX HERE]\n"
-		    "[PARTIAL - AF'/BC'/DE'/HL'/IY HERE]\n"
-		    "PC =%04x SP =%04x F=[????????]\n"
-		    "[PARTIAL - IM/IFF1/IFF2/I/R HERE]\n",
-		    env->pc,
-		    env->regs[R_SP]
-		    );
+    int fl = env->regs[R_F];
+
+    cpu_fprintf(f, "AF =%04x BC =%04x DE =%04x HL =%04x IX=%04x\n"
+                    "AF'=%04x BC'=%04x DE'=%04x HL'=%04x IY=%04x\n"
+                    "PC =%04x SP =%04x F=[%c%c%c%c%c%c%c%c]\n"
+                    "[PARTIAL - IM/IFF1/IFF2/I/R HERE]\n",
+
+                    (env->regs[R_A] << 8) | env->regs[R_F],
+                    env->regs[R_BC], env->regs[R_DE],
+                    env->regs[R_HL], env->regs[R_IX],
+
+                    (env->regs[R_AX] << 8) | env->regs[R_FX],
+                    env->regs[R_BCX], env->regs[R_DEX],
+                    env->regs[R_HLX], env->regs[R_IY],
+
+                    env->pc, env->regs[R_SP],
+                    fl & 0x80 ? 'S' : '-',
+                    fl & 0x40 ? 'Z' : '-',
+                    fl & 0x20 ? 'Y' : '-',
+                    fl & 0x10 ? 'H' : '-',
+                    fl & 0x08 ? 'X' : '-',
+                    fl & 0x04 ? 'P' : '-',
+                    fl & 0x02 ? 'N' : '-',
+                    fl & 0x01 ? 'C' : '-'
+                    );
 #else   /* final repo.or.cz implementation */
     int fl = env->regs[R_F];
     cpu_fprintf(f, "AF =%04x BC =%04x DE =%04x HL =%04x IX=%04x\n"
