4c80c81484f7e636ec06927640f6c3ef22549558
William Towle <william_towle@yahoo.co.uk>
2021-02-21 12:58:10 +0000

    WIP: handle v2 style in/out

--- target/z80/helper.h
+++ target/z80/helper.h
@@ -28,6 +28,14 @@ DEF_HELPER_2(raise_exception, void, env, int)
 DEF_HELPER_2(movl_pc_im, void, env, i32)
 
 
+/* In/Out */
+
+DEF_HELPER_2(in_T0_im, void, env, i32)
+DEF_HELPER_1(in_T0_bc_cc, void, env)
+DEF_HELPER_2(out_T0_im, void, env, i32)
+DEF_HELPER_1(out_T0_bc, void, env)
+
+
 /* Misc */
 
 DEF_HELPER_2(bit_T0, void, env, i32)
