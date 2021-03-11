4dc3cf986cdfc1d1fb6c07187211396787040dd5
William Towle <william_towle@yahoo.co.uk>
2021-03-11 19:17:06 +0000

    WIP: Implement z80_cpu_get_phys_page_debug()

--- target/z80/cpu.c
+++ target/z80/cpu.c
@@ -216,10 +216,6 @@ static void z80_cpu_set_pc(CPUState *cs, vaddr value)
     cpu->env.pc = value;
 }
 
-/* TODO:
- * z80_cpu_get_phys_page_debug() via helper.c
- */
-
 //static void z80_cpu_disas_set_info(CPUState *cpu, disassemble_info *info)
 //{
 //;DPRINTF("DEBUG: Reached %s() ** PARTIAL **\n", __func__);
@@ -271,8 +267,9 @@ static void z80_cpu_class_init(ObjectClass *oc, void *data)
 //    cc->gdb_write_register = z80_cpu_gdb_write_register;
 //    cc->gdb_num_core_regs = 35;	/* NUMBER_OF_CPU_REGISTERS? */
 //    cc->gdb_core_xml_file = "z80-cpu.xml";
-    /* TODO: set get_phys_page_debug if !CONFIG_USER_ONLY */
-//    cc->get_phys_page_debug = z80_cpu_get_phys_page_debug;
+#if !defined(CONFIG_USER_ONLY)
+    cc->get_phys_page_debug = z80_cpu_get_phys_page_debug;
+#endif
 //    cc->vmsd = &vmstate_z80_cpu;
     /* TODO? i386 adjusts CPU eflags with:
      * cc->cpu_exec_enter
