0d0ffe92ada7be627b88bb4b63e359529802a4eb
William Towle <william_towle@yahoo.co.uk>
2021-03-14 17:09:37 +0000

    WIP: add zaphod_uart.[ch]

---
+++ hw/z80/zaphod_uart.c
@@ -0,0 +1,13 @@
+/*
+ * QEmu Zaphod board - UART support
+ * vim: ft=c sw=4 ts=4 et :
+ *
+ * [...William Towle, under GPL...]
+ */
+
+//#include "zaphod.h"
+
+//#include "qemu/error-report.h"
+
+//#define DPRINTF(fmt, ...) \
+//    do { if (ZAPHOD_DEBUG) error_printf("zaphod_uart: " fmt , ## __VA_ARGS__); } while(0)
---
+++ hw/z80/zaphod_uart.h
@@ -0,0 +1,15 @@
+/*
+ * QEmu Zaphod board - UART support
+ * vim: ft=c sw=4 ts=4 et :
+ *
+ * [...William Towle c. 2013-2022, under GPL...]
+ */
+
+#ifndef HW_Z80_ZAPHOD_UART_H
+#define HW_Z80_ZAPHOD_UART_H
+
+//#include "zaphod.h"
+
+/* TODO: typedefs and prototypes here */
+
+#endif  /* HW_Z80_ZAPHOD_UART_H */
