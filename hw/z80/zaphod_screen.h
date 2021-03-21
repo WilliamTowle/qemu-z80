064ca24b49b28ee318f0e604e17d883c61721f92
William Towle <william_towle@yahoo.co.uk>
2021-03-21 16:55:37 +0000

    Add zaphod_screen.[ch]

---
+++ hw/z80/zaphod_screen.h
@@ -0,0 +1,17 @@
+/*
+ * QEmu Zaphod board - screen support
+ * vim: ft=c sw=4 ts=4 et :
+ *
+ * [...William Towle c. 2013-2022, under GPL...]
+ */
+
+
+#ifndef HW_Z80_ZAPHOD_SCREEN_H
+#define HW_Z80_ZAPHOD_SCREEN_H
+
+//#include "zaphod.h"
+
+
+/* TODO: class/state typedef and prototypes */
+
+#endif  /* HW_Z80_ZAPHOD_SCREEN_H */
