037ec2748a91483b56580b05c1052f3d741dccc1
William Towle <william_towle@yahoo.co.uk>
2021-03-21 16:55:37 +0000

    Add zaphod_screen.[ch]

---
+++ hw/z80/zaphod_screen.c
@@ -0,0 +1,19 @@
+/*
+ * QEmu Zaphod board - screen support
+ * vim: ft=c sw=4 ts=4 et :
+ *
+ * [...William Towle, under GPL...]
+ */
+
+
+//#include "qemu-osdep.h"
+//#include "zaphod.h"
+
+
+//#define EMIT_DEBUG ZAPHOD_DEBUG
+//#define EMIT_DEBUG 0
+//#define DPRINTF(fmt, ...) \
+//    do { if (EMIT_DEBUG) error_printf("zaphod_screen: " fmt , ## __VA_ARGS__); } while(0)
+
+
+/* TODO: TypeInfo and registration for ZaphodScreenClass */
