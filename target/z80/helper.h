12cfd6cdfcab112a3d0ee86223d8e1caba55f79a
William Towle <william_towle@yahoo.co.uk>
2021-03-24 21:58:31 +0000

    WIP: towards arithmetic op support - xor helper

--- target/z80/helper.h
+++ target/z80/helper.h
@@ -23,3 +23,16 @@ DEF_HELPER_2(movl_pc_im, void, env, int)
 /* Misc */
 
 DEF_HELPER_1(jmp_T0, void, env)
+
+
+/* 8-bit arithmetic ops */
+
+//DEF_HELPER_0(add_cc, void)
+//DEF_HELPER_0(adc_cc, void)
+//DEF_HELPER_0(sub_cc, void)
+//DEF_HELPER_0(sbc_cc, void)
+//DEF_HELPER_0(and_cc, void)
+//DEF_HELPER_0(xor_cc, void)
+DEF_HELPER_1(xor_cc, void, env)
+//DEF_HELPER_0(or_cc, void)
+//DEF_HELPER_0(cp_cc, void)
--- target/z80/op_helper.c
+++ target/z80/op_helper.c
@@ -20,6 +20,42 @@
     do { if (EMIT_DEBUG) error_printf("Z80 op_helper: " fmt , ## __VA_ARGS__); } while(0)
 
 
+const uint8_t parity_table[256] = {
+    CC_P, 0, 0, CC_P, 0, CC_P, CC_P, 0,
+    0, CC_P, CC_P, 0, CC_P, 0, 0, CC_P,
+    0, CC_P, CC_P, 0, CC_P, 0, 0, CC_P,
+    CC_P, 0, 0, CC_P, 0, CC_P, CC_P, 0,
+    0, CC_P, CC_P, 0, CC_P, 0, 0, CC_P,
+    CC_P, 0, 0, CC_P, 0, CC_P, CC_P, 0,
+    CC_P, 0, 0, CC_P, 0, CC_P, CC_P, 0,
+    0, CC_P, CC_P, 0, CC_P, 0, 0, CC_P,
+    0, CC_P, CC_P, 0, CC_P, 0, 0, CC_P,
+    CC_P, 0, 0, CC_P, 0, CC_P, CC_P, 0,
+    CC_P, 0, 0, CC_P, 0, CC_P, CC_P, 0,
+    0, CC_P, CC_P, 0, CC_P, 0, 0, CC_P,
+    CC_P, 0, 0, CC_P, 0, CC_P, CC_P, 0,
+    0, CC_P, CC_P, 0, CC_P, 0, 0, CC_P,
+    0, CC_P, CC_P, 0, CC_P, 0, 0, CC_P,
+    CC_P, 0, 0, CC_P, 0, CC_P, CC_P, 0,
+    0, CC_P, CC_P, 0, CC_P, 0, 0, CC_P,
+    CC_P, 0, 0, CC_P, 0, CC_P, CC_P, 0,
+    CC_P, 0, 0, CC_P, 0, CC_P, CC_P, 0,
+    0, CC_P, CC_P, 0, CC_P, 0, 0, CC_P,
+    CC_P, 0, 0, CC_P, 0, CC_P, CC_P, 0,
+    0, CC_P, CC_P, 0, CC_P, 0, 0, CC_P,
+    0, CC_P, CC_P, 0, CC_P, 0, 0, CC_P,
+    CC_P, 0, 0, CC_P, 0, CC_P, CC_P, 0,
+    CC_P, 0, 0, CC_P, 0, CC_P, CC_P, 0,
+    0, CC_P, CC_P, 0, CC_P, 0, 0, CC_P,
+    0, CC_P, CC_P, 0, CC_P, 0, 0, CC_P,
+    CC_P, 0, 0, CC_P, 0, CC_P, CC_P, 0,
+    0, CC_P, CC_P, 0, CC_P, 0, 0, CC_P,
+    CC_P, 0, 0, CC_P, 0, CC_P, CC_P, 0,
+    CC_P, 0, 0, CC_P, 0, CC_P, CC_P, 0,
+    0, CC_P, CC_P, 0, CC_P, 0, 0, CC_P,
+};
+
+
 /* NB. set_inhibit_irq() not invoked
  * repo.or.cz doesn't make it clear what should have called this (but
  * does this explain FIXMEs against 'retn'/'reti' insns?)
@@ -48,4 +84,16 @@ void helper_movl_pc_im(CPUZ80State *env, int new_pc)
 void helper_jmp_T0(CPUZ80State *env)
 {
     PC = T0;
+}
+
+//void HELPER(xor_cc)(void)
+void helper_xor_cc(CPUZ80State *env)
+{
+    int sf, zf, pf;
+    A = (uint8_t)(A ^ T0);
+
+    sf = (A & 0x80) ? CC_S : 0;
+    zf = A ? 0 : CC_Z;
+    pf = parity_table[(uint8_t)A];
+    F = sf | zf | pf;
 }
