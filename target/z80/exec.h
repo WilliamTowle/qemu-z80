73dcbd551143064e1981a7de203f73e9c2e87f06
William Towle <william_towle@yahoo.co.uk>
2021-03-24 21:58:31 +0000

    WIP: exec.h register defines

--- target/z80/exec.h
+++ target/z80/exec.h
@@ -10,5 +10,26 @@
 #ifndef Z80_EXEC_H
 #define Z80_EXEC_H
 
+/* Access to registers/emulator temporary values */
+
+#define T0 (env->t0)
+//#define T1 (env->t1)
+//#define A   (env->regs[R_A])
+//#define F   (env->regs[R_F])
+//#define BC  (env->regs[R_BC])
+//#define DE  (env->regs[R_DE])
+//#define HL  (env->regs[R_HL])
+//#define IX  (env->regs[R_IX])
+//#define IY  (env->regs[R_IY])
+//#define SP  (env->regs[R_SP])
+//#define I   (env->regs[R_I])
+//#define R   (env->regs[R_R])
+//#define AX  (env->regs[R_AX])
+//#define FX  (env->regs[R_FX])
+//#define BCX (env->regs[R_BCX])
+//#define DEX (env->regs[R_DEX])
+//#define HLX (env->regs[R_HLX]
+
+#define PC	(env->pc)
 
 #endif	/* Z80_EXEC_H */
