69d6128d8fc61f4e17c9a29fa258e67c995325d5
William Towle <william_towle@yahoo.co.uk>
2021-03-24 17:30:28 +0000

    target/z80: heirloom style exec.h
    This header contains material from repo.or.cz related to instruction
    helper implementation.

---
+++ target/z80/exec.h
@@ -0,0 +1,14 @@
+/*
+ * QEmu Z80 CPU - execution defines
+ * vim: ft=c sw=4 ts=4 et :
+ *
+ *  Copyright (c) 2007-2009 Stuart Brady <stuart.brady@gmail.com>
+ *  Ported/provisional reimplementations by William Towle 2018-2022
+ *  All versions under GPL
+ */
+
+#ifndef Z80_EXEC_H
+#define Z80_EXEC_H
+
+
+#endif	/* Z80_EXEC_H */
