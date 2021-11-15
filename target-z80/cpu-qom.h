403813fd3adf40a849042dc394354da48993f532
William Towle <william_towle@yahoo.co.uk>
2021-11-15 18:56:07 +0000

    target-z80: TYPE_Z80_CPU and Z80CPUClass

--- target-z80/cpu-qom.h
+++ target-z80/cpu-qom.h
@@ -13,6 +13,32 @@
 #include "cpu.h"
 //#include "qapi/error.h"
 
+#define TYPE_Z80_CPU "z80-cpu"
+
+#define Z80_CPU_CLASS(klass) \
+    OBJECT_CLASS_CHECK(Z80CPUClass, (klass), TYPE_Z80_CPU)
+#define Z80_CPU(obj) \
+    OBJECT_CHECK(Z80CPU, (obj), TYPE_Z80_CPU)
+#define Z80_CPU_GET_CLASS(obj) \
+    OBJECT_GET_CLASS(Z80CPUClass, (obj), TYPE_Z80_CPU)
+
+
+/**
+ * Z80CPUClass:
+ * @parent_realize: The parent class' realize handler.
+ * @parent_reset: The parent class' reset handler.
+ *
+ * A Z80 CPU model or family.
+ */
+typedef struct Z80CPUClass {
+    /*< private >*/
+    CPUClass parent_class;
+    /*< public >*/
+
+    DeviceRealize parent_realize;
+    void (*parent_reset)(CPUState *cpu);
+} Z80CPUClass;
+
 
 /**
  * Z80CPU:
