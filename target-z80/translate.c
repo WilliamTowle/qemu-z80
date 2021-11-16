623dce15033c45dfaf2f9f39b5c1f501aa80a0aa
William Towle <william_towle@yahoo.co.uk>
2021-11-16 09:41:45 +0000

    WIP: target-z80 - skeleton restore_state_to_opc()

--- target-z80/translate.c
+++ target-z80/translate.c
@@ -38,3 +38,32 @@ void gen_intermediate_code_pc(CPUZ80State *env, TranslationBlock *tb)
 {
     gen_intermediate_code_internal(z80_env_get_cpu(env), tb, true);
 }
+
+void restore_state_to_opc(CPUZ80State *env, TranslationBlock *tb, int pc_pos)
+{
+#if 1
+;DPRINTF("%s() UNIMPLEMENTED\n", __func__);
+;exit(1);
+#else   /* Suitable for X86 */
+    int cc_op;
+#ifdef DEBUG_DISAS
+    if (qemu_loglevel_mask(CPU_LOG_TB_OP)) {
+        int i;
+        qemu_log("RESTORE:\n");
+        for(i = 0;i <= pc_pos; i++) {
+            if (tcg_ctx.gen_opc_instr_start[i]) {
+                qemu_log("0x%04x: " TARGET_FMT_lx "\n", i,
+                        tcg_ctx.gen_opc_pc[i]);
+            }
+        }
+        qemu_log("pc_pos=0x%x eip=" TARGET_FMT_lx " cs_base=%x\n",
+                pc_pos, tcg_ctx.gen_opc_pc[pc_pos] - tb->cs_base,
+                (uint32_t)tb->cs_base);
+    }
+#endif
+    env->eip = tcg_ctx.gen_opc_pc[pc_pos] - tb->cs_base;
+    cc_op = gen_opc_cc_op[pc_pos];
+    if (cc_op != CC_OP_DYNAMIC)
+        env->cc_op = cc_op;
+#endif
+}
