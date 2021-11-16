8b57c66fe2d7039b1e3594b106465f2d8b162797
William Towle <william_towle@yahoo.co.uk>
2021-11-16 10:19:32 +0000

    WIP: toward CPU init - register types

--- target-z80/cpu.c
+++ target-z80/cpu.c
@@ -94,3 +94,62 @@ out:
     g_strfreev(model_pieces);
     return cpu;
 }
+
+
+static void z80_cpu_class_init(ObjectClass *oc, void *data)
+{
+    DeviceClass *dc = DEVICE_CLASS(oc);
+    CPUClass *cc = CPU_CLASS(oc);
+    Z80CPUClass *zcc = Z80_CPU_CLASS(oc);
+
+#if 1	/* WmT - PARTIAL */
+;fprintf(stderr, "DEBUG: Reached %s() **PARTIAL ONLY**\n", __func__);
+#endif
+    zcc->parent_realize = dc->realize;
+    dc->realize = z80_cpu_realizefn;
+
+//    device_class_set_parent_reset(dc, z80_cpu_reset, &zcc->parent_reset);
+//
+//    cc->class_by_name = z80_cpu_class_by_name;
+//
+    cc->has_work = z80_cpu_has_work;
+//    cc->do_interrupt = z80_cpu_do_interrupt;
+//    cc->cpu_exec_interrupt = z80_cpu_exec_interrupt;
+//    cc->dump_state = z80_cpu_dump_state;
+    cc->set_pc = z80_cpu_set_pc;
+//    cc->memory_rw_debug = z80_cpu_memory_rw_debug;
+//    cc->get_phys_page_debug = z80_cpu_get_phys_page_debug;
+//    cc->tlb_fill = z80_cpu_tlb_fill;
+////    cc->vmsd = &vms_z80_cpu;
+//    cc->disas_set_info = z80_cpu_disas_set_info;
+//    cc->tcg_initialize = z80_cpu_tcg_init;
+    cc->synchronize_from_tb = z80_cpu_synchronize_from_tb;
+//    cc->gdb_read_register = z80_cpu_gdb_read_register;
+//    cc->gdb_write_register = z80_cpu_gdb_write_register;
+//    cc->gdb_num_core_regs = 35;	/* NUMBER_OF_CPU_REGISTERS? */
+//    cc->gdb_core_xml_file = "z80-cpu.xml";
+}
+
+static const TypeInfo z80_cpu_type_info = {
+    /* NB: default to regular Zilog Z80 for now [TODO: use 'name' of
+     * TYPE_Z80_CPU for the base class and Z80_CPU_TYPE_NAME(x) for
+     * the Z80/R800 subclasses and in z80_cpu_type_name() likewise]
+     */
+    .name = TYPE_Z80_CPU,
+    //.name = Z80_CPU_TYPE_NAME(TYPE_Z80_CPU),
+    .parent = TYPE_CPU,
+    .instance_size = sizeof(Z80CPU),
+    .instance_init = z80_cpu_instance_init,
+    //.abstract = true,
+    .abstract = false,
+    .class_size = sizeof(Z80CPUClass),
+    .class_init = z80_cpu_class_init,
+};
+
+static void z80_cpu_register_types(void)
+{
+    type_register_static(&z80_cpu_type_info);
+}
+
+
+type_init(z80_cpu_register_types)
