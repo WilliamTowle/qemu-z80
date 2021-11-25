c661e0c6e96bbafb0df244aae03c2684792d1528
William Towle <william_towle@yahoo.co.uk>
2021-11-25 18:40:35 +0000

    translate.c: mark gen_*() unused variables

--- target-z80/translate.c
+++ target-z80/translate.c
@@ -644,10 +644,10 @@ static inline void gen_cond_jump(int cc, int l1)
 static inline void gen_jcc(DisasContext *s, int cc,
                            target_ulong val, target_ulong next_pc)
 {
-    TranslationBlock *tb;
+    //TranslationBlock *tb; /* unused */
     int l1;
 
-    tb = s->tb;
+    //tb = s->tb;
 
     l1 = gen_new_label();
 
@@ -664,10 +664,10 @@ static inline void gen_jcc(DisasContext *s, int cc,
 static inline void gen_callcc(DisasContext *s, int cc,
                               target_ulong val, target_ulong next_pc)
 {
-    TranslationBlock *tb;
+    //TranslationBlock *tb; /* unused */
     int l1;
 
-    tb = s->tb;
+    //tb = s->tb;
 
     l1 = gen_new_label();
 
@@ -686,10 +686,10 @@ static inline void gen_callcc(DisasContext *s, int cc,
 static inline void gen_retcc(DisasContext *s, int cc,
                              target_ulong next_pc)
 {
-    TranslationBlock *tb;
+    //TranslationBlock *tb; /* unused */
     int l1;
 
-    tb = s->tb;
+    //tb = s->tb;
 
     l1 = gen_new_label();
 
