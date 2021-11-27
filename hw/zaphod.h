aec6abb83667c0a7f63454f67a2c9bd687835264
William Towle <william_towle@yahoo.co.uk>
2021-11-27 23:24:10 +0000

    Minimal ("v1 style") zaphod init
    Styled after dummy_m68k.c - initialises just CPU and RAM (and then
    bails - otherwise capable of executing 'nop's from empty RAM)

---
+++ hw/zaphod.h
@@ -0,0 +1,19 @@
+/*
+ * QEmu Zaphod machine family
+ * vim: ft=c sw=4 ts=4 et :
+ *
+ * [...William Towle c. 2013-2021, under GPL...]
+ */
+
+#ifndef _ZAPHOD_H_
+#define _ZAPHOD_H_
+
+#define ZAPHOD_DEBUG	1
+
+/* Z80_MAX_RAM_SIZE:
+ * Address space for a Z80 ends at 64K (some emulations might use less)
+ */
+#define	Z80_MAX_RAM_SIZE	(64 * 1024) /* 64KiB */
+
+
+#endif	/*  _ZAPHOD_H_  */
