e46c1ecf21ce5b04dc0adf0f97826dbdcb734468
William Towle <william_towle@yahoo.co.uk>
2021-12-02 13:11:24 +0000

    TEMP: exec.h for v1.0,1
    This header is obsolete, with content needing to be refactored.
    Introduce a file so that 'blame' can be used to identify remaining
    commits that modify it.

---
+++ target-z80/exec.h
@@ -0,0 +1,15 @@
+/*
+ * QEmu Z80 virtual CPU
+ * vim: ft=c sw=4 ts=4 et :
+ *
+ * [...Stuart Brady/William Towle, under GPL...]
+ */
+
+#ifndef __TARGET_Z80_EXEC_H_
+#define __TARGET_Z80_EXEC_H_
+
+/* FIXME. This header is obsolete, and exists to catch commits
+ * that modify it
+ */
+
+#endif	/* __TARGET_Z80_EXEC_H_ */
