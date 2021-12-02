f219fc4cc1710442894b8799aaf80b37216d01b7
William Towle <william_towle@yahoo.co.uk>
2021-12-02 13:32:39 +0000

    WIP: op_helper.c helpers need dyngen-exec.h include

--- target-z80/op_helper.c
+++ target-z80/op_helper.c
@@ -18,8 +18,10 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA  02110-1301 USA
  */
 
+#include "cpu.h"
 #include "exec.h"
 #include "helper.h"
+#include "dyngen-exec.h"
 
 //#define A0 (env->a0)
 //
