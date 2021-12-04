4b878db4057b32d43e548fc9f08602d1c4344671
William Towle <william_towle@yahoo.co.uk>
2021-12-04 10:35:08 +0000

    WIP: Rework headers/op_helper.c for v1.0,1

--- target-z80/op_helper.c
+++ target-z80/op_helper.c
@@ -20,11 +20,15 @@
  */
 
 #include "cpu.h"
-#include "exec.h"
+//#include "exec.h"
 #include "helper.h"
 #include "dyngen-exec.h"
-
 #include "ioport.h"
+
+#if !defined(CONFIG_USER_ONLY)
+#include "softmmu_exec.h"
+#endif /* !defined(CONFIG_USER_ONLY) */
+
 
 #if 1  /* was: TARGET_LONG_BITS > HOST_LONG_BITS
         * but TCG_AREG{1|2} no longer available for else case
@@ -43,23 +47,24 @@ target_ulong T1;
 //register target_ulong T1 asm(AREG2);
 #endif /* ! (TARGET_LONG_BITS > HOST_LONG_BITS) */
 
-//#define A0 (env->a0)
-//
-//#define A   (env->regs[R_A])
-//#define F   (env->regs[R_F])
-//#define BC  (env->regs[R_BC])
-//#define DE  (env->regs[R_DE])
-//#define HL  (env->regs[R_HL])
-//#define IX  (env->regs[R_IX])
-//#define IY  (env->regs[R_IY])
-//#define SP  (env->regs[R_SP])
-//#define I   (env->regs[R_I])
-//#define R   (env->regs[R_R])
-//#define AX  (env->regs[R_AX])
-//#define FX  (env->regs[R_FX])
-//#define BCX (env->regs[R_BCX])
-//#define DEX (env->regs[R_DEX])
-//#define HLX (env->regs[R_HLX])
+
+#define A0 (env->a0)
+
+#define A   (env->regs[R_A])
+#define F   (env->regs[R_F])
+#define BC  (env->regs[R_BC])
+#define DE  (env->regs[R_DE])
+#define HL  (env->regs[R_HL])
+#define IX  (env->regs[R_IX])
+#define IY  (env->regs[R_IY])
+#define SP  (env->regs[R_SP])
+#define I   (env->regs[R_I])
+#define R   (env->regs[R_R])
+#define AX  (env->regs[R_AX])
+#define FX  (env->regs[R_FX])
+#define BCX (env->regs[R_BCX])
+#define DEX (env->regs[R_DEX])
+#define HLX (env->regs[R_HLX])
 
 #define PC  (env->pc)
 
