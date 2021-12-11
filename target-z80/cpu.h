f85a2ee5dc09ee39a81fc6ba318188877ab4a105
William Towle <william_towle@yahoo.co.uk>
2021-12-11 01:00:13 +0000

    WIP: target-z80 - temp'y new "minimal" CPU files

---
+++ target-z80/cpu.h
@@ -0,0 +1,11 @@
+/*
+ * Skeleton for minimal QEmu Z80 CPU
+ * vim: ft=c sw=4 ts=4 et :
+ *
+ * WmT, 2018-2022 [...after Stuart Brady, under GPL...]
+ */
+
+#ifndef CPU_Z80_H
+#define CPU_Z80_H
+
+#endif /* !defined (CPU_Z80_H) */
---
+++ target-z80/op_helper.c
@@ -0,0 +1,6 @@
+/*
+ * Skeleton for minimal QEmu Z80 CPU
+ * vim: ft=c sw=4 ts=4 et :
+ *
+ * WmT, 2018-2022 [...after Stuart Brady, under GPL...]
+ */
