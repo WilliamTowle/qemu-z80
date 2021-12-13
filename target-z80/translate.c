3482dd41dfb8b78be33e3ae367064d2477484308
William Towle <william_towle@yahoo.co.uk>
2021-12-13 13:54:25 +0000

    WIP: implement singlestep check

--- target-z80/translate.c
+++ target-z80/translate.c
@@ -1170,16 +1170,14 @@ static inline int gen_intermediate_code_internal(CPUState *env,
         /* if irq were inhibited with HF_INHIBIT_IRQ_MASK, we clear
            the flag and abort the translation to give the irqs a
            change to be happen */
-#if 1	/* WmT - TRACE */
-;DPRINTF("[%s:%d] PARTIAL - unimplemented 'singlestep'...\n", __FILE__, __LINE__);
-#else
+
         if (dc->singlestep_enabled ||
             (flags & HF_INHIBIT_IRQ_MASK)) {
             gen_jmp_im(pc_ptr - dc->cs_base);
             gen_eob(dc);
             break;
         }
-#endif
+
         /* if too long translation, stop generation too */
         if (gen_opc_ptr >= gen_opc_end ||
             (pc_ptr - pc_start) >= (TARGET_PAGE_SIZE - 32) ||
