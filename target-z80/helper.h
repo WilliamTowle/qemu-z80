c348a431f7c65ed10646c06c5730dec958689ffe
William Towle <william_towle@yahoo.co.uk>
2022-01-22 19:27:03 +0000

    WIP: restore template's includes in helper.h

--- target-z80/helper.h
+++ target-z80/helper.h
@@ -1,6 +1,6 @@
-#include "def-helper.h"
+#include "exec/def-helper.h"
 
 //DEF_HELPER_1(movl_pc_im, void, i32)
 DEF_HELPER_2(movl_pc_im, void, env, i32)
 
-#include "def-helper.h"
+#include "exec/def-helper.h"
