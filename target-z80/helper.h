e098927a2c3f641d09938d54910748fa4668ddfc
William Towle <william_towle@yahoo.co.uk>
2022-01-23 12:22:31 +0000

    WIP: IRQ/debug helpers to v1.7.x style

--- target-z80/helper.h
+++ target-z80/helper.h
@@ -1,6 +1,6 @@
 #include "exec/def-helper.h"
 
-DEF_HELPER_0(debug, void)
+DEF_HELPER_1(debug, void, env)
 //DEF_HELPER_1(raise_exception, void, i32)
 DEF_HELPER_3(raise_interrupt, void, env, int, int)
 DEF_HELPER_2(raise_exception, void, env, int)
