f3a67581dc44a1828a7db5f021b9ef776cfbfca4
William Towle <william_towle@yahoo.co.uk>
2022-01-23 11:06:20 +0000

    WIP: translate.c for v1.7.x - no 'return' here

--- target-z80/translate.c
+++ target-z80/translate.c
@@ -1259,7 +1259,6 @@ static inline void gen_intermediate_code_internal(Z80CPU *cpu,
 #if 1	/* WmT - TRACE */
 ;DPRINTF("*** EXIT %s(), OK ***\n", __func__);
 #endif
-    return 0;
 }
 
 void gen_intermediate_code(CPUZ80State *env, TranslationBlock *tb)
