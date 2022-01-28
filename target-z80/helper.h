d6c03eb87ca66a6239a719cef6da413389bd47d9
William Towle <william_towle@yahoo.co.uk>
2022-01-28 20:21:42 +0000

    WIP: jmp_T0 helper to v1.7.x style

--- target-z80/helper.h
+++ target-z80/helper.h
@@ -16,7 +16,7 @@ DEF_HELPER_2(movl_pc_im, void, env, i32)
 /* Misc */
 
 //DEF_HELPER_1(bit_T0, void, i32)
-DEF_HELPER_0(jmp_T0, void)
+DEF_HELPER_1(jmp_T0, void, env)
 //DEF_HELPER_2(djnz, void, i32, i32)
 
 #include "exec/def-helper.h"
