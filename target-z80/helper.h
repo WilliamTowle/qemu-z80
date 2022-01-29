6462494783c9a1fcea3ece455b2aa65ffd4533eb
William Towle <william_towle@yahoo.co.uk>
2022-01-29 16:30:05 +0000

    WIP: v1.7.x - fix op_helper/translate build

--- target-z80/helper.h
+++ target-z80/helper.h
@@ -14,10 +14,10 @@ DEF_HELPER_2(movl_pc_im, void, env, i32)
 DEF_HELPER_1(halt, void, env)
 
 /* In / Out */
-DEF_HELPER_1(in_T0_im, void, i32)
-DEF_HELPER_0(in_T0_bc_cc, void)
-DEF_HELPER_1(out_T0_im, void, i32)
-DEF_HELPER_0(out_T0_bc, void)
+DEF_HELPER_2(in_T0_im, void, env, i32)
+DEF_HELPER_1(in_T0_bc_cc, void, env)
+DEF_HELPER_2(out_T0_im, void, env, i32)
+DEF_HELPER_1(out_T0_bc, void, env)
 
 /* Misc */
 DEF_HELPER_2(bit_T0, void, env, i32)
