b353c565c0d9b74d95c91b99c838ebe140bdf3f5
William Towle <william_towle@yahoo.co.uk>
2022-01-29 12:16:19 +0000

    v1.7.x compat: fix op_helper.c register access macros

--- target-z80/op_helper.c
+++ target-z80/op_helper.c
@@ -41,23 +41,23 @@ target_ulong T1;
 //register target_ulong T1 asm(AREG2);
 #endif /* ! (TARGET_LONG_BITS > HOST_LONG_BITS) */
 
-//#define A0 (env->a0)
-//
-//#define A   (env->regs[R_A])
-//#define F   (env->regs[R_F])
-//#define BC  (env->regs[R_BC])
-//#define DE  (env->regs[R_DE])
-//#define HL  (env->regs[R_HL])
-//#define IX  (env->regs[R_IX])
-//#define IY  (env->regs[R_IY])
-//#define SP  (env->regs[R_SP])
-//#define I   (env->regs[R_I])
-//#define R   (env->regs[R_R])
-//#define AX  (env->regs[R_AX])
-//#define FX  (env->regs[R_FX])
-//#define BCX (env->regs[R_BCX])
-//#define DEX (env->regs[R_DEX])
-//#define HLX (env->regs[R_HLX])
+#define A0 (env->a0)
+
+#define A   (env->regs[R_A])
+#define F   (env->regs[R_F])
+#define BC  (env->regs[R_BC])
+#define DE  (env->regs[R_DE])
+#define HL  (env->regs[R_HL])
+#define IX  (env->regs[R_IX])
+#define IY  (env->regs[R_IY])
+#define SP  (env->regs[R_SP])
+#define I   (env->regs[R_I])
+#define R   (env->regs[R_R])
+#define AX  (env->regs[R_AX])
+#define FX  (env->regs[R_FX])
+#define BCX (env->regs[R_BCX])
+#define DEX (env->regs[R_DEX])
+#define HLX (env->regs[R_HLX])
 
 #define PC  (env->pc)
 
