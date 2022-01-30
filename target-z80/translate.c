901df69c92ff96e30019471fbfbf8d52e95cd496
William Towle <william_towle@yahoo.co.uk>
2022-01-30 10:12:54 +0000

    WIP: v1.7.x - r800 insns in new style

--- target-z80/translate.c
+++ target-z80/translate.c
@@ -1401,7 +1401,7 @@ next_byte:
                     /* does mulub work with r1 == h, l, (hl) or a? */
                     r1 = regmap(reg[y], m);
                     gen_movb_v_reg(cpu_T[0], r1);
-                    gen_helper_mulub_cc();
+                    gen_helper_mulub_cc(cpu_env);
                     zprintf("mulub a,%s\n", regnames[r1]);
                     break;
                 case 3:
@@ -1410,7 +1410,7 @@ next_byte:
                         /* what is the effect of DD/FD prefixes here? */
                         r1 = regpairmap(regpair[p], m);
                         gen_movw_v_reg(cpu_T[0], r1);
-                        gen_helper_muluw_cc();
+                        gen_helper_muluw_cc(cpu_env);
                         zprintf("muluw hl,%s\n", regpairnames[r1]);
                     } else {
                         zprintf("nop\n");
