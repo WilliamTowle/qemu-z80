17002a698ec66bbeffe66c86416f5a7773e5dbe5
William Towle <william_towle@yahoo.co.uk>
2022-02-01 18:40:38 +0000

    WIP: putchar non-printable character handling

--- hw/z80/zaphod.c
+++ hw/z80/zaphod.c
@@ -76,7 +76,28 @@ void zaphod_inkey_receive(void *opaque, const uint8_t *buf, int len)
 
 static void zaphod_sercon_putchar(ZaphodState *zs, const unsigned char ch)
 {
-    zs->sercon->chr_write(zs->sercon, &ch, 1);
+    if (isprint(ch))
+    {
+        zs->sercon->chr_write(zs->sercon, &ch, 1);
+    }
+    else
+    {   /* Render non-printable characters as corresponding hex.
+         * TODO: pass all characters to device-specific handler to
+         * interpret appropriately
+         */
+        uint8_t nyb_hi, nyb_lo;
+
+        nyb_hi= (ch & 0xf0) >> 4;
+        nyb_lo= ch & 0x0f;
+
+        zaphod_sercon_putchar(zs, '[');
+        nyb_hi+= (nyb_hi > 9)? 'A' - 10 : '0';
+        zaphod_sercon_putchar(zs, nyb_hi);
+        nyb_lo+= (nyb_lo > 9)? 'A' - 10 : '0';
+        zaphod_sercon_putchar(zs, nyb_lo);
+        zaphod_sercon_putchar(zs, ']');
+        zaphod_sercon_putchar(zs, '*');
+    }
 }
 
 static void zaphod_putchar(ZaphodState *zs, const unsigned char ch)
