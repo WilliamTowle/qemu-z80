0a4502eecd74285537b92130672864b3762523f5
William Towle <william_towle@yahoo.co.uk>
2022-02-01 23:04:55 +0000

    Zaphod: configure/build for separate zaphod_sercon

---
+++ hw/z80/zaphod_sercon.h
@@ -0,0 +1,15 @@
+/*
+ * QEmu Zaphod board - sercon support
+ * vim: ft=c sw=4 ts=4 et :
+ *
+ * [...William Towle c. 2013-2022, under GPL...]
+ */
+
+#ifndef HW_Z80_ZAPHOD_SERCON_H
+#define HW_Z80_ZAPHOD_SERCON_H
+
+//#include "zaphod.h"
+
+/* TODO: migrate typedefs and prototypes here */
+
+#endif  /* HW_Z80_ZAPHOD_SERCON_H */
