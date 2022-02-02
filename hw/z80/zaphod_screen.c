9e4886de080893367781d854f4407934f96dae45
William Towle <william_towle@yahoo.co.uk>
2022-02-02 19:18:41 +0000

    WIP: v1.7.x - "zaphod_screen init/build"

---
+++ hw/z80/zaphod_screen.c
@@ -0,0 +1,20 @@
+/*
+ * QEmu Zaphod board - screen support
+ * vim: ft=c sw=4 ts=4 et :
+ *
+ * [...William Towle c. 2013-2022, under GPL...]
+ */
+
+#include "zaphod.h"
+
+//#include "qemu/error-report.h"
+
+
+//#ifdef ZAPHOD_DEBUG
+//	/* TODO: version with error_printf() needs CPU headers */
+//#define DPRINTF(fmt, ...) \
+//	do { fprintf(stderr, fmt, ## __VA_ARGS__); } while (0)
+//#else
+//#define DPRINTF(fmt, ...) \
+//	do { } while (0)
+//#endif
