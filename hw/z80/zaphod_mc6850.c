9992d4a5972971c795ab51788f738e2737cd960a
William Towle <william_towle@yahoo.co.uk>
2022-02-02 20:48:06 +0000

    Zaphod: configure/build for MC6850 (serial) device

---
+++ hw/z80/zaphod_mc6850.c
@@ -0,0 +1,18 @@
+/*
+ * QEmu Zaphod board - Motorola MC6850 ACIA support
+ * vim: ft=c sw=4 ts=4 et :
+ *
+ * [...William Towle c. 2013-2022, under GPL...]
+ */
+
+//#include "zaphod.h"
+
+//#include "qemu/error-report.h"
+//
+//#ifdef ZAPHOD_DEBUG
+//#define DPRINTF(fmt, ...) \
+//    do { error_printf( "zaphod: " fmt , ## __VA_ARGS__); } while (0)
+//#else
+//#define DPRINTF(fmt, ...) \
+//    do { } while (0)
+//#endif
