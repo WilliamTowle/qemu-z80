83ec111be55d5aa065fa98e9f4f28380d7fff5bd
William Towle <william_towle@yahoo.co.uk>
2022-02-02 20:32:36 +0000

    WIP: "zaphod_screen: set screen foreground by machine type"

--- hw/z80/zaphod.c
+++ hw/z80/zaphod.c
@@ -101,13 +101,22 @@ static void zaphod_init_common(ZaphodState *zs, QEMUMachineInitArgs *args)
     zs->sercon= zaphod_new_sercon(zs, serial_hds[0]);
 #endif
 #ifdef ZAPHOD_HAS_SCREEN
-    zs->screen= zaphod_new_screen();
+    zs->screen= zaphod_new_screen(zs);
 #endif
 }
 
 static void zaphod_add_feature(ZaphodState *zs, zaphod_feature_t n)
 {
-    zs->features|= 1 << n;
+    switch(n)
+    {
+#ifdef ZAPHOD_HAS_SCREEN
+    case ZAPHOD_SIMPLE_SCREEN:
+#endif
+        zs->features|= 1 << n;
+        break;
+    default:
+        DPRINTF("%s(): unsupported/missing feature with n=%d requested\n", __func__, n);
+    }
 }
 
 int zaphod_has_feature(ZaphodState *zs, zaphod_feature_t n)
@@ -144,7 +153,7 @@ static QEMUMachine zaphod_dev_machine= {
 
 static void zaphod_init_pb_machine(QEMUMachineInitArgs *args)
 {
-    ZaphodState *zs= g_new(ZaphodState, 1);
+    ZaphodState *zs= g_new0(ZaphodState, 1);
     zaphod_add_feature(zs, ZAPHOD_SIMPLE_SCREEN);
     zaphod_init_common(zs, args);
 }
