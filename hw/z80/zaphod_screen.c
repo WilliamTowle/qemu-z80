f27513e346855ae24d7b8f657d653d1626d28337
William Towle <william_towle@yahoo.co.uk>
2022-02-02 19:29:18 +0000

    zaphod_screen: realize function with console init

--- hw/z80/zaphod_screen.c
+++ hw/z80/zaphod_screen.c
@@ -8,18 +8,49 @@
 #include "qemu/osdep.h"
 #include "zaphod.h"
 
-//#include "qemu/error-report.h"
+#include "ui/console.h"
 
 
-//#ifdef ZAPHOD_DEBUG
-//	/* TODO: version with error_printf() needs CPU headers */
-//#define DPRINTF(fmt, ...) \
-//	do { fprintf(stderr, fmt, ## __VA_ARGS__); } while (0)
-//#else
-//#define DPRINTF(fmt, ...) \
-//	do { } while (0)
-//#endif
+/* Implements a monochrome display at 25x80 text resolution (Phil
+ * Brown calls his Amiga emulator screen "medium-res two-colour
+ * 24x80" [Amiga is usually 25x80 at 640x200 for NTSC and 32x80 at
+ * 640x256 for PAL].
+ * Skeleton support for Grant Searle's ATmega328 per-line attributes
+ * (normally bold/size/graphics) is present. For graphics, each byte
+ * of the line maps to a 2x4 pixel subgroup of all 160x100 pixels
+ */
 
+#ifdef ZAPHOD_DEBUG
+	/* TODO: version with error_printf() needs CPU headers */
+#define DPRINTF(fmt, ...) \
+	do { fprintf(stderr, fmt, ## __VA_ARGS__); } while (0)
+#else
+#define DPRINTF(fmt, ...) \
+	do { } while (0)
+#endif
+
+
+//#include "vgafont.h"		/* vgafont16 - 16x8 */
+#define FONT_HEIGHT	16
+#define FONT_WIDTH	8
+#define ZAPHOD_TEXT_ROWS	25
+#define ZAPHOD_TEXT_COLS	80
+
+
+static void zaphod_screen_invalidate_display(void *opaque)
+{
+;DPRINTF("[%s:%d] Reached UNIMPLEMENTED %s()\n", __FILE__, __LINE__, __func__);
+}
+
+static void zaphod_screen_update_display(void *opaque)
+{
+;DPRINTF("[%s:%d] Reached UNIMPLEMENTED %s()\n", __FILE__, __LINE__, __func__);
+}
+
+static const GraphicHwOps zaphod_screen_ops= {
+    .invalidate     = zaphod_screen_invalidate_display,
+    .gfx_update     = zaphod_screen_update_display,
+};
 
 DeviceState *zaphod_screen_new(void)
 {
@@ -33,13 +64,35 @@ DeviceState *zaphod_screen_new(void)
     return dev;
 }
 
+
+static void zaphod_screen_realizefn(DeviceState *dev, Error **errp)
+{
+    ZaphodScreenState *zss= ZAPHOD_SCREEN(dev);
+
+    /* NB. our text mode is essentially VGA-like; is QEmu's
+     * common display code useful?
+     */
+    zss->display= graphic_console_init(
+                NULL,	/* no ISA bus bus to emulate */
+                &zaphod_screen_ops,
+                zss);
+
+    /* TODO: calls to zaphod_consolegui_invalidate_display() (eg. when
+     * user switches to HMI and back) need to prompt a full redraw of
+     * previous output
+     */
+    qemu_console_resize(zss->display,
+        FONT_WIDTH * ZAPHOD_TEXT_COLS, FONT_HEIGHT * ZAPHOD_TEXT_ROWS);
+
+    /* TODO: assign reset function here for initialisation */
+}
+
 static void zaphod_screen_class_init(ObjectClass *klass, void *data)
 {
     DeviceClass *dc = DEVICE_CLASS(klass);
 
-#if 0  /* TODO: v1 init support */
     dc->realize = zaphod_screen_realizefn;
-    /* TODO: initialisation in dc->reset? */
+#if 0  /* TODO: v1 init support */
     dc->props = zaphod_screen_properties;
 #endif
     set_bit(DEVICE_CATEGORY_DISPLAY, dc->categories);
