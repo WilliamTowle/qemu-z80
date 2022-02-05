1f6f07cfe75a88310d41c164fb6c3ca750c59f64
William Towle <william_towle@yahoo.co.uk>
2022-02-05 02:48:34 +0000

    zaphod_screen: introduce "dirty" region for dpy_update()s

--- hw/z80/zaphod_screen.h
+++ hw/z80/zaphod_screen.h
@@ -31,6 +31,8 @@ typedef struct {
     bool            cursor_visible;
     int64_t         cursor_blink_time;    /* millisec */
     uint8_t         *rgb_bg, *rgb_fg;
+    int             dirty_minr, dirty_maxr;
+    int             dirty_minc, dirty_maxc;
 } ZaphodScreenState;
 
 #define TYPE_ZAPHOD_SCREEN "zaphod-screen"
