302dc5022848e410ca2e871ebdab02567fd50d08
William Towle <william_towle@yahoo.co.uk>
2022-02-05 03:27:31 +0000

    WIP: v1.7.x - use sane cpu_interrupt() args

--- hw/z80/zaphod.c
+++ hw/z80/zaphod.c
@@ -152,9 +152,8 @@ uint8_t zaphod_get_inkey(void *opaque, bool read_and_clear)
 void zaphod_interrupt(void *opaque, int source, int level)
 {
     ZaphodState  *zs= (ZaphodState *)opaque;
-
-
-    cpu_interrupt(zs->cpu, CPU_INTERRUPT_HARD);
+    CPUState *cs = CPU(z80_env_get_cpu(zs->cpu));
+    cpu_interrupt(cs, CPU_INTERRUPT_HARD);
 }
 #endif
 
