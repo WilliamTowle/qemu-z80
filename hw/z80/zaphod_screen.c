25d765ba4d4960c9ae65d8c1f56717db21df980b
William Towle <william_towle@yahoo.co.uk>
2022-02-05 15:47:16 +0000

    WIP: v1.7.x - fix 'surface' use

--- hw/z80/zaphod_screen.c
+++ hw/z80/zaphod_screen.c
@@ -137,7 +137,7 @@ void zaphod_screen_draw_char(void *opaque, int row, int col, char ch)
         DPRINTF("INFO: Unexpected state in %s() - bypp != 4\n", __func__);
         return;
     }
-    else if (unlikely (is_surface_bgr(zss->ds->surface)))
+    else if (unlikely(is_surface_bgr(surface)))
     {
         DPRINTF("INFO: Unexpected state in %s() - is_surface_bgr() true\n", __func__);
         return;
@@ -192,7 +192,7 @@ void zaphod_screen_draw_graphic(void *opaque, int row, int col, uint8_t data)
         DPRINTF("INFO: Unexpected state in %s() - bypp != 4\n", __func__);
         return;
     }
-    else if (unlikely (is_surface_bgr(zss->ds->surface)))
+    else if (unlikely(is_surface_bgr(surface)))
     {
         DPRINTF("INFO: Unexpected state in %s() - is_surface_bgr() true\n", __func__);
         return;
