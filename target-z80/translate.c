8bf8cc3405cc6148537ecf922528367b65e25e43
William Towle <william_towle@yahoo.co.uk>
2022-02-25 20:53:53 +0000

    HEADERS: "Full+Porting" for translate.c

--- target-z80/translate.c
+++ target-z80/translate.c
@@ -1,10 +1,23 @@
 /*
- * Skeleton for minimal QEmu Z80 CPU
- * vim: ft=c sw=4 ts=4 et :
+ * Z80 translation
  *
- * WmT, 2018-2022 [...after Stuart Brady, under GPLv2+...]
+ *  Copyright (c) 2007-2009 Stuart Brady <stuart.brady@gmail.com>
+ *  Porting by William Towle 2018-2022
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA  02110-1301 USA
  */
-
 
 #include <stdio.h>
 
