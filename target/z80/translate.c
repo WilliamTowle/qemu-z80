4c33b9a7e15529c1c88375ff0681cc762ee46548
William Towle <william_towle@yahoo.co.uk>
2022-04-23 15:57:38 +0100

    WIP: translate.c 0x21 'ld' implement

--- target/z80/translate.c
+++ target/z80/translate.c
@@ -423,6 +423,13 @@ static const int reg[8] = {
     OR_A
 };
 
+static const int regpair[4]= {
+    OR2_BC,
+    OR2_DE,
+    OR2_HL,
+    OR2_SP,
+};
+
 
 static inline void gen_jmp_im(target_ulong pc)
 {
@@ -634,8 +641,36 @@ static target_ulong disas_insn(DisasContext *s, CPUState *cpu)
         switch (x)
         {
         case 0:      /* insn pattern 00yyyzzz */
-            switch (z) {
-            /* TODO: case(s) for z=0 to z=5 */
+            switch (z)
+            {
+            /* TODO: case for z=0 */
+            case 1:
+                switch (q) {
+                case 0:	/* 0x21 */
+#if 1   /* WmT - TRACE */
+;DPRINTF("[%s:%d] GETTING HERE?\n", __FILE__, __LINE__);
+;exit(1);
+#else
+                    n = z80_lduw_code(env, s);
+                    //s->pc+= 2;
+                    tcg_gen_movi_tl(cpu_T[0], n);
+                    r1 = regpairmap(regpair[p], m);
+                    gen_movw_reg_v(r1, cpu_T[0]);
+                    zprintf("ld %s,$%04x\n", regpairnames[r1], n);
+#endif
+                    break;
+
+                /* TODO: case for q=1 */
+
+                default:    /* PARTIAL: switch(q) incomplete */
+#if 1   /* WmT - TRACE */
+;DPRINTF("[%s:%d] FALLTHROUGH - MODE_%s op 0x%02x (x %o, y %o [p=%o/q=%o], z %d) unhandled q case\n", __FILE__, __LINE__, (m == MODE_NORMAL)?"NORMAL":"xD", b, x, y,p,q, z);
+#endif
+                    goto unknown_op;
+                }
+                break;
+
+            /* TODO: case(s) for z=2 to z=5 */
 
             case 6: /* 0x3e */
                 r1 = regmap(reg[y], m);
