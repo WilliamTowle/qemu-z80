9f45f5a7c7b2eaf7aba8c7751cd4b017bd5effa6
William Towle <william_towle@yahoo.co.uk>
2022-05-08 21:29:21 +0100

    WIP: restore bblbrx-user main.c variables

--- bblbrx-user/main.c
+++ bblbrx-user/main.c
@@ -20,10 +20,10 @@
 
 
 unsigned long guest_base;
-//unsigned long reserved_va;
+unsigned long reserved_va;
 
 __thread CPUState *thread_cpu;
-//int singlestep;
+int singlestep;
 
 
 bool qemu_cpu_is_self(CPUState *cpu)
