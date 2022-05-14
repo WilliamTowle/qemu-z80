4060b22ac473ef30666e165ac35d26f53735e8ee
William Towle <william_towle@yahoo.co.uk>
2022-05-14 18:29:01 +0100

    WIP: enable path to 10yyyzzz arithmetic

--- target/z80/translate.c
+++ target/z80/translate.c
@@ -777,10 +777,6 @@ static target_ulong disas_insn(DisasContext *s, CPUState *cpu)
          */
 
         case 2: /* insn pattern 10yyyzzz - arithmetic/logic */
-#if 1   /* WmT - TRACE */
-;DPRINTF("[%s:%d] GETTING HERE?\n", __FILE__, __LINE__);
-;exit(1);
-#else
             r1 = regmap(reg[z], m);
             if (is_indexed(r1)) {
                 d = z80_ldsb_code(env, s);
@@ -796,7 +792,6 @@ static target_ulong disas_insn(DisasContext *s, CPUState *cpu)
                 zprintf("%s%s\n", alu[y], regnames[r1]);
             }
             break;
-#endif
 
         case 3: /* insn pattern 11yyyzzz */
             switch (z) {
