0a3e9f54daaf77a889a84bc1e2fc1794f7dd86fe
William Towle <william_towle@yahoo.co.uk>
2022-05-24 16:06:44 +0100

    v2 compat: refine "MMU" management

--- target/z80/cpu.h
+++ target/z80/cpu.h
@@ -55,6 +55,11 @@
 #define CPUArchState struct CPUZ80State
 
 
+#define PG_ERROR_W_BIT     1
+
+#define EXCP0E_PAGE	14
+
+
 /* Array indexes for registers.
  * NB: corresponding z80-cpu.xml "gdb target" file with register sizes
  * has not been written
@@ -119,7 +124,7 @@ enum {
 
 #define CPU_NB_REGS 15
 
-//#define NB_MMU_MODES 1
+#define NB_MMU_MODES 1
 
 
 /* CPUZ80State */
@@ -161,7 +166,6 @@ struct Z80CPU {
 };
 
 
-/* helper.c */
 #if !defined(CONFIG_USER_ONLY)
 hwaddr z80_cpu_get_phys_page_debug(CPUState *cs, vaddr addr);
 #endif
@@ -186,6 +190,9 @@ void z80_cpu_list(FILE *f, fprintf_function cpu_fprintf);
 
 int cpu_z80_signal_handler(int host_signum, void *pinfo, void *puc);
 
+/* excp_helper.c */
+int z80_cpu_handle_mmu_fault(CPUState *cpu, vaddr addr, int size,
+                             int is_write, int mmu_idx);
 
 #define TARGET_PAGE_BITS 8
 
