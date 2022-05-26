9163c25fa651814a6045ca57e8b0ac0673a2700a
William Towle <william_towle@yahoo.co.uk>
2022-05-26 13:09:57 +0100

    Minimal zaphod board init - CPU, RAM

--- hw/z80/zaphod.h
+++ hw/z80/zaphod.h
@@ -18,7 +18,14 @@
 //#include "hw/boards.h"
 
 
-/* TODO: Z80_MAX_RAM_SIZE and related defines */
+/* Z80_MAX_RAM_SIZE:
+ * Address space for a Z80 ends at 64K (some emulations might use less)
+ */
+#if QEMU_VERSION_MAJOR < 3  /* "KiB" from qemu/units.h in QEmu v3+ */
+#define KiB                 1024
+#endif
+#define Z80_MAX_RAM_SIZE    (64 * KiB)
+
 
 /* TODO: ZaphodMachineClass, ZaphodMachineState types here */
 
