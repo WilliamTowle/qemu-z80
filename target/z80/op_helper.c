77f60ef3d1f6196c4864df9afaaf617a2a29f17b
William Towle <william_towle@yahoo.co.uk>
2022-06-10 20:28:38 +0100

    WIP: IRQ inhibit helper(s)

--- target/z80/op_helper.c
+++ target/z80/op_helper.c
@@ -20,6 +20,25 @@
     do { if (EMIT_DEBUG) error_printf("Z80 op_helper: " fmt , ## __VA_ARGS__); } while(0)
 
 
+/* NB. set_inhibit_irq() not invoked
+ * repo.or.cz doesn't make it clear what should have called this (but
+ * does this explain FIXMEs against 'retn'/'reti' insns?)
+ * In target-i386 for QEmu v1, calls occurred:
+ * - on pop of ES/SS/DS
+ * - on 'mov seg, Gv'
+ * - on 'sti'
+ */
+//void HELPER(set_inhibit_irq)(CPUZ80State *env)
+//{
+//    env->hflags |= HF_INHIBIT_IRQ_MASK;
+//}
+
+void HELPER(reset_inhibit_irq)(CPUZ80State *env)
+{
+    env->hflags &= ~HF_INHIBIT_IRQ_MASK;
+}
+
+
 void helper_movl_pc_im(CPUZ80State *env, int new_pc)
 {
     PC= (uint16_t)new_pc;
