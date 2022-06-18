1a278634b8f3ee5bdceefc3d841bf430093f0b62
William Towle <william_towle@yahoo.co.uk>
2022-06-18 11:42:06 +0100

    WIP: add zaphod_iocore.[ch]

---
+++ hw/z80/zaphod_iocore.h
@@ -0,0 +1,15 @@
+/*
+ * QEmu Zaphod board - iocore support
+ * vim: ft=c sw=4 ts=4 et :
+ *
+ * [...William Towle, under GPL...]
+ */
+
+#ifndef HW_Z80_ZAPHOD_IOCORE_H
+#define HW_Z80_ZAPHOD_IOCORE_H
+
+//#include "zaphod.h"
+
+/* TODO: typedefs and prototypes here */
+
+#endif  /* HW_Z80_ZAPHOD_IOCORE_H */
