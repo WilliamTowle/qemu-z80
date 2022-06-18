fd56d200ec4bc5a6327c92d60d9b1a54d0bc1996
William Towle <william_towle@yahoo.co.uk>
2022-06-18 11:42:06 +0100

    WIP: add zaphod_iocore.[ch]

---
+++ hw/z80/zaphod_iocore.c
@@ -0,0 +1,19 @@
+/*
+ * QEmu Zaphod board - IOCore support
+ * vim: ft=c sw=4 ts=4 et :
+ *
+ * [...William Towle, under GPL...]
+ */
+
+
+//#include "qemu-osdep.h"
+//#include "zaphod.h"
+
+//#include "qemu/error-report.h"
+
+//#define EMIT_DEBUG ZAPHOD_DEBUG
+//#define DPRINTF(fmt, ...) \
+//    do { if (EMIT_DEBUG) error_printf("zaphod_iocore: " fmt , ## __VA_ARGS__); } while(0)
+
+
+/* TODO: TypeInfo and registration for ZaphodIOCoreClass */
