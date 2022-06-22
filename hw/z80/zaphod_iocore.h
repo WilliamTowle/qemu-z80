8d1d94c2a8fc00e699052616dd3eb176371d737b
William Towle <william_towle@yahoo.co.uk>
2022-06-22 15:53:57 +0100

    zaphod_iocore: qdevified init, with 'inkey' notes

--- hw/z80/zaphod_iocore.h
+++ hw/z80/zaphod_iocore.h
@@ -2,15 +2,33 @@
  * QEmu Zaphod board - IOCore support
  * vim: ft=c sw=4 ts=4 et :
  *
- * [...William Towle, under GPL...]
+ * [...William Towle c. 2013-2022, under GPL...]
  */
 
 #ifndef HW_Z80_ZAPHOD_IOCORE_H
 #define HW_Z80_ZAPHOD_IOCORE_H
 
-//#include "zaphod.h"
+#include "zaphod.h"
 
-/* TODO: class/state typedef and prototypes here */
 
+typedef DeviceClass ZaphodIOCoreClass;
+
+typedef struct {
+    DeviceState     parent;
+    //CharBackend   chr;
+} ZaphodIOCoreState;
+
+
+#define TYPE_ZAPHOD_IOCORE "zaphod-iocore"
+
+#define ZAPHOD_IOCORE_GET_CLASS(obj) \
+    OBJECT_GET_CLASS(ZaphodIOCoreClass, obj, TYPE_ZAPHOD_IOCORE)
+#define ZAPHOD_IOCORE_CLASS(klass) \
+    OBJECT_CLASS_CHECK(ZaphodIOCoreClass, klass, TYPE_ZAPHOD_IOCORE)
+#define ZAPHOD_IOCORE(obj) \
+    OBJECT_CHECK(ZaphodIOCoreState, obj, TYPE_ZAPHOD_IOCORE)
+
+
+ZaphodIOCoreState *zaphod_iocore_init(void);
 
 #endif  /* HW_Z80_ZAPHOD_IOCORE_H */
