761ef345769030a8fb6f1b2bbf49d648d02fb497
William Towle <william_towle@yahoo.co.uk>
2022-06-22 20:36:36 +0100

    zaphod_iocore: init w/ awareness of host board

--- hw/z80/zaphod.h
+++ hw/z80/zaphod.h
@@ -50,7 +50,7 @@ typedef struct {
 } ZaphodMachineClass;
 #endif
 
-typedef struct {
+struct ZaphodMachineState {
     /*< private >*/
     MachineState parent;
 
@@ -62,7 +62,7 @@ typedef struct {
 #ifdef CONFIG_ZAPHOD_HAS_UART
     ZaphodUARTState     *uart_stdio;
 #endif
-} ZaphodMachineState;
+};
 
 
 #define TYPE_ZAPHOD_MACHINE \
