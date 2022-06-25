be60ca6f282f90ec27628672f0d3eea565bce6b8
William Towle <william_towle@yahoo.co.uk>
2022-06-25 17:28:51 +0100

    zaphod_mc6850: device init with set_bit()s

--- hw/z80/zaphod.h
+++ hw/z80/zaphod.h
@@ -27,6 +27,11 @@
 #include "zaphod_sercon.h"
 #endif
 
+#ifdef ZAPHOD_HAS_MC6850
+#include "zaphod_mc6850.h"
+#endif
+
+
 /* Z80_MAX_RAM_SIZE:
  * Address space for a Z80 ends at 64K (some emulations might use less)
  */
@@ -35,12 +40,6 @@
 #endif
 #define	Z80_MAX_RAM_SIZE	(64 * KiB)
 
-
-#ifdef ZAPHOD_HAS_MC6850
-typedef struct {
-    PortioList *ports;
-} ZaphodMC6850State;
-#endif
 
 
 typedef enum {                  /* "features" bit map bit offsets */
