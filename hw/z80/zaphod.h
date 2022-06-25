f87888ff0968fe6444d570c61bc3309da4162a94
William Towle <william_towle@yahoo.co.uk>
2022-06-25 22:37:23 +0100

    zaphod_screen: qdevified device init

--- hw/z80/zaphod.h
+++ hw/z80/zaphod.h
@@ -28,9 +28,9 @@
 #ifdef CONFIG_ZAPHOD_HAS_UART
 #include "zaphod_uart.h"
 #endif
-//#ifdef CONFIG_ZAPHOD_HAS_SCREEN
-//#include "zaphod_screen.h"
-//#endif
+#ifdef CONFIG_ZAPHOD_HAS_SCREEN
+#include "zaphod_screen.h"
+#endif
 
 
 /* Z80_MAX_RAM_SIZE:
@@ -65,6 +65,10 @@ struct ZaphodMachineState {
 #endif
 #ifdef CONFIG_ZAPHOD_HAS_UART
     ZaphodUARTState     *uart_stdio;
+#endif
+
+#ifdef CONFIG_ZAPHOD_HAS_SCREEN
+    ZaphodScreenState    *screen;
 #endif
 };
 
