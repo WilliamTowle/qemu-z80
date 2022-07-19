ce30b9ba01e352eee6d99dea194fcbf27b77c6d4
William Towle <william_towle@yahoo.co.uk>
2022-07-19 16:38:16 +0100

    WIP: basic zaphod_uart.c

--- hw/z80/zaphod_uart.h
+++ hw/z80/zaphod_uart.h
@@ -2,7 +2,7 @@
  * QEmu Zaphod board - UART support
  * vim: ft=c sw=4 ts=4 et :
  *
- * [...William Towle, under GPL...]
+ * [...William Towle c. 2013-2022, under GPL...]
  */
 
 #ifndef HW_Z80_ZAPHOD_UART_H
@@ -10,7 +10,38 @@
 
 //#include "zaphod.h"
 
-/* TODO: class|state typedef and prototypes here */
+#include "chardev/char-fe.h"
+#include "exec/ioport.h"
+
+
+typedef DeviceClass ZaphodUARTClass;
+
+typedef struct {
+    DeviceState     parent;
+
+    CharBackend     chr;
+    uint8_t         inkey;
+} ZaphodUARTState;
+
+
+#define TYPE_ZAPHOD_UART "zaphod-uart"
+
+#define ZAPHOD_UART_GET_CLASS(obj) \
+    OBJECT_GET_CLASS(ZaphodUARTClass, obj, TYPE_ZAPHOD_UART)
+#define ZAPHOD_UART_CLASS(klass) \
+    OBJECT_CLASS_CHECK(ZaphodUARTClass, klass, TYPE_ZAPHOD_UART)
+#define ZAPHOD_UART(obj) \
+    OBJECT_CHECK(ZaphodUARTState, obj, TYPE_ZAPHOD_UART)
+
+
+#if 0
+uint8_t zaphod_sercon_get_inkey(void *opaque, bool read_and_clear);
+void zaphod_sercon_set_inkey(void *opaque, uint8_t val, bool is_data);
+#endif
+
+
+//DeviceState *zaphod_uart_new(Chardev *chr);
+DeviceState *zaphod_uart_new(void);
 
 
 #endif  /* HW_Z80_ZAPHOD_UART_H */
