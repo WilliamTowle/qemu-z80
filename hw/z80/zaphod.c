d04a23a9a440686cf2b380158fc8447ba6f2ad32
William Towle <william_towle@yahoo.co.uk>
2022-07-19 19:30:09 +0100

    WIP: don't hardcode serial_hds[0] inside zaphod_uart

--- hw/z80/zaphod.c
+++ hw/z80/zaphod.c
@@ -108,9 +108,10 @@ static void zaphod_generic_board_init(MachineState *ms)
 
     if (serial_hds[0])
     {   /* QEmu's serial console exists */
-#if 1   /* WmT - TRACE */
-;DPRINTF("TODO: non-NULL serial0 is available - do UART init!\n");
-#endif
+;DPRINTF("INFO: initialising UART device...\n");
+        //zms->uart_stdio= ZAPHOD_UART(zaphod_uart_new());
+        zms->uart_stdio= ZAPHOD_UART(zaphod_uart_new(serial_hds[0]));
+;DPRINTF("INFO: UART device created OK, at %p\n", zms->uart_stdio);
     }
 
 #ifdef CONFIG_ZAPHOD_HAS_IOCORE
