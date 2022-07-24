57390f3903dfbee0fc5ed95c0dd38d8014078b7e
William Towle <william_towle@yahoo.co.uk>
2022-07-24 20:33:10 +0100

    zaphod.c: declare, initialise uart_acia

--- hw/z80/zaphod.h
+++ hw/z80/zaphod.h
@@ -75,6 +75,7 @@ struct ZaphodMachineState {
 #endif
 #ifdef CONFIG_ZAPHOD_HAS_UART
     ZaphodUARTState     *uart_stdio;
+    ZaphodUARTState     *uart_acia;
 #endif
 
 #ifdef CONFIG_ZAPHOD_HAS_SCREEN
