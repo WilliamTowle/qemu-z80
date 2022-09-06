1b75177cbeb5b3ff83cf443c6777d6d8fb9e1e0d
William Towle <william_towle@yahoo.co.uk>
2022-09-06 18:35:04 +0100

    misc_helper.c: add debug helper

--- target/z80/misc_helper.c
+++ target/z80/misc_helper.c
@@ -5,3 +5,23 @@
  *  Ported/provisional reimplementations by William Towle 2018-2022
  *  All versions under GPL
  */
+
+#include "qemu/osdep.h"
+#include "cpu.h"
+
+#include "exec/helper-proto.h"
+#include "exec/exec-all.h"
+
+
+void helper_debug(CPUZ80State *env)
+{
+    CPUState *cs = CPU(z80_env_get_cpu(env));
+
+    cs->exception_index = EXCP_DEBUG;
+#if QEMU_VERSION_MAJOR < 2  /* v0.15.0 style */
+    cpu_loop_exit(env);
+#else
+    /* trigger call to cpu_handle_debug_exception() */
+    cpu_loop_exit(cs);
+#endif
+}
