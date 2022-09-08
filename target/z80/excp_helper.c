afe7e88e196fe85a4f85f48c259723c281eb9887
William Towle <william_towle@yahoo.co.uk>
2022-09-08 17:42:13 +0100

    WIP: toward interrupts simplify

--- target/z80/excp_helper.c
+++ target/z80/excp_helper.c
@@ -40,8 +40,7 @@
  */
 static void QEMU_NORETURN raise_interrupt2(CPUZ80State *env, int intno,
                                            int is_int, int error_code,
-                                           int next_eip_addend,
-                                           uintptr_t retaddr)
+                                           int next_eip_addend)
 {
     CPUState *cs = CPU(z80_env_get_cpu(env));
 
@@ -61,14 +60,13 @@ static void QEMU_NORETURN raise_interrupt2(CPUZ80State *env, int intno,
 #if 0	/* i386 specific */
     env->exception_next_eip = env->eip + next_eip_addend;
 #endif
-    cpu_loop_exit_restore(cs, retaddr);
+    cpu_loop_exit(cs);
 }
 
 void raise_exception(CPUZ80State *env, int exception_index)
 {
-    raise_interrupt2(env, exception_index, 0, 0, 0, 0);
+    raise_interrupt2(env, exception_index, 0, 0, 0);
 }
-
 
 void helper_raise_exception(CPUZ80State *env, int exception_index)
 {
