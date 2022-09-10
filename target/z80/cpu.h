f00f37f3ae365b023d242b3dbb46aa40b595c8b8
William Towle <william_towle@yahoo.co.uk>
2022-09-10 21:42:20 +0100

    WIP: add, support 'model' in CPUZ80State

--- target/z80/cpu.h
+++ target/z80/cpu.h
@@ -151,7 +151,7 @@ typedef struct CPUZ80State {
     struct {} end_reset_fields;
     /* Fields after this point are preserved across CPU reset. */
 
-    /* TODO: identifier for CPU model */
+    int model;
 } CPUZ80State;
 
 
@@ -173,6 +173,9 @@ bool z80_cpu_exec_interrupt(CPUState *cpu, int int_req);
 hwaddr z80_cpu_get_phys_page_debug(CPUState *cs, vaddr addr);
 #endif
 
+
+#define Z80_CPU_Z80     1
+#define Z80_CPU_R800    2
 
 static inline Z80CPU *z80_env_get_cpu(CPUZ80State *env)
 {
