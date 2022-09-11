8009df6cbe5afdd761ba2688f1187363e74050ee
William Towle <william_towle@yahoo.co.uk>
2022-09-11 14:22:04 +0100

    WIP: rework TARGET_DEFAULT_CPU_TYPE

--- target/z80/cpu.h
+++ target/z80/cpu.h
@@ -185,7 +185,8 @@ bool z80_cpu_tlb_fill(CPUState *cs, vaddr address, int size,
 #define Z80_CPU_TYPE_NAME(name) (name Z80_CPU_TYPE_SUFFIX)
 #define CPU_RESOLVING_TYPE TYPE_Z80_CPU
 
-#define TARGET_DEFAULT_CPU_TYPE TYPE_Z80_CPU
+#define TARGET_DEFAULT_CPU_TYPE Z80_CPU_TYPE_NAME("z80");
+//#define TARGET_DEFAULT_CPU_TYPE TYPE_Z80_CPU
 
 
 #define cpu_signal_handler cpu_z80_signal_handler
