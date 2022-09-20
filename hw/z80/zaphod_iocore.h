872b048c05e6b5cea2c9c2c163cc743f2dca6780
William Towle <william_towle@yahoo.co.uk>
2022-09-20 15:48:06 +0100

    zaphod_iocore: track press of shift key

--- hw/z80/zaphod_iocore.h
+++ hw/z80/zaphod_iocore.h
@@ -32,6 +32,7 @@ typedef struct {
     //bool          has_acia;       /* mc6850, w/ interrupts */
 #if 1   /* IOCORE-KEYBIO */
     QemuInputHandlerState *ihs;
+    int             modifiers;
 #endif
 } ZaphodIOCoreState;
 
