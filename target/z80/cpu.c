b0a1fdc96a7b708b90ccd3a5f74e30e1c559d88c
William Towle <william_towle@yahoo.co.uk>
2022-09-23 09:47:41 +0100

    WIP: enable cpu.c DPRINTF

--- target/z80/cpu.c
+++ target/z80/cpu.c
@@ -11,6 +11,6 @@
 #include "qemu/error-report.h"
 
 //#define EMIT_DEBUG ZAPHOD_DEBUG
-#define EMIT_DEBUG 0
+#define EMIT_DEBUG 1
 #define DPRINTF(fmt, ...) \
     do { if (EMIT_DEBUG) error_printf("Z80 cpu: " fmt , ## __VA_ARGS__); } while(0)
