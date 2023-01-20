662c8bcf42e5ca39dc081ca3bed8abf162d60683
William Towle <william_towle@yahoo.co.uk>
2023-01-20 21:49:47 +0000

    v5 compat: fix helper_debug()

--- target/z80/misc_helper.c
+++ target/z80/misc_helper.c
@@ -15,7 +15,7 @@
 
 void helper_debug(CPUZ80State *env)
 {
-    CPUState *cs = CPU(z80_env_get_cpu(env));
+    CPUState *cs = env_cpu(env);
 
     cs->exception_index = EXCP_DEBUG;
 #if QEMU_VERSION_MAJOR < 2  /* v0.15.0 style */
