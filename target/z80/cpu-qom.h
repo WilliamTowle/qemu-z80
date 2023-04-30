1060ee9008741b94c6d6b2e556b9f3128439ce7a
William Towle <william_towle@yahoo.co.uk>
2023-04-30 15:04:50 +0100

    HEADERS: cpu-qom.h - full

--- target/z80/cpu-qom.h
+++ target/z80/cpu-qom.h
@@ -2,9 +2,23 @@
  * QEmu Z80 CPU
  * vim: ft=c sw=4 ts=4 et :
  *
- *  Ported/provisional reimplementations by William Towle 2018-2022
- *  All versions under GPL
+ *  Copyright (c) 2018-2023 William Towle <william_towle@yahoo.co.uk>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA  02110-1301 USA
  */
+
 
 #ifndef QEMU_Z80_CPU_QOM_H
 #define QEMU_Z80_CPU_QOM_H
