22d46eb2ed168c65dc7702e09aa10f818c3c54d5
William Towle <william_towle@yahoo.co.uk>
2023-07-18 20:23:08 +0100

    SQUASHME: more interrupts simplify/tidy

--- target/z80/excp_helper.c
+++ target/z80/excp_helper.c
@@ -27,33 +27,19 @@
  * is_int is TRUE.
  */
 static void QEMU_NORETURN raise_interrupt2(CPUZ80State *env, int intno,
-                                           int is_int, int error_code,
-                                           int next_eip_addend)
+                                           int is_int, int error_code)
 {
     CPUState *cs = CPU(z80_env_get_cpu(env));
-
-#if 0   /* x86-specific */
-    if (!is_int) {
-        cpu_svm_check_intercept_param(env, SVM_EXIT_EXCP_BASE + intno,
-                                      error_code, retaddr);
-        intno = check_exception(env, intno, &error_code, retaddr);
-    } else {
-        cpu_svm_check_intercept_param(env, SVM_EXIT_SWINT, 0, retaddr);
-    }
-#endif
 
     cs->exception_index = intno;
     env->error_code = error_code;
     env->exception_is_int = is_int;
-#if 0   /* i386 specific */
-    env->exception_next_eip = env->eip + next_eip_addend;
-#endif
     cpu_loop_exit(cs);
 }
 
 void raise_exception(CPUZ80State *env, int exception_index)
 {
-    raise_interrupt2(env, exception_index, 0, 0, 0);
+    raise_interrupt2(env, exception_index, 0, 0);
 }
 
 void helper_raise_exception(CPUZ80State *env, int exception_index)
